38bc151dc37a834062965b11f1ee98450108bbe5
Aaron Bockover <abockover@novell.com>
2009-12-20 18:09:32 -0500

    [build] add configure options to mono

--- packages/mono.py
+++ packages/mono.py
@@ -1,7 +1,19 @@
+configure_flags = [
+	'--with-jit=yes',
+	'--with-ikvm=no',
+	'--with-mcs-docs=no',
+	'--with-moonlight=no',
+	'--enable-quiet-build'
+]
+
 package = {
 	'name':    'mono',
 	'version': '2.6.1',
 	'sources': [
 		'http://ftp.novell.com/pub/%{name}/sources/%{name}/%{name}-%{version}.tar.bz2'
+	],
+	'build': [
+		'%{__configure} ' + ' '.join (configure_flags),
+		'%{__make}'
 	]
 }
