84922b0c7a761a6f0c453eb2e6e1affa62f5dfc7
Aaron Bockover <abockover@novell.com>
2009-12-29 08:09:49 -0500

    [build] build gtk-sharp from svn, fix up .configs
    The gtk/gdk-sharp-2.0 assembly config files map to the x11 variant
    of the GDK/GTK native libraries, so after make install we change
    them to map to quartz.

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -1,8 +1,31 @@
+def svn_co_or_up (package):
+	os.chdir ('..')
+	if os.path.isdir ('gtk-sharp-svn'):
+		os.chdir ('gtk-sharp-svn')
+		os.system ('svn up')
+	else:
+		os.system ('svn co http://anonsvn.mono-project.com/source/trunk/gtk-sharp gtk-sharp-svn')
+		os.chdir ('gtk-sharp-svn')
+	os.chdir ('..')
+
 package = {
 	'name':    'gtk-sharp',
-	'version': '2.12.9',
+	'version': 'svn.HEAD',
 	'branch':  '212',
 	'sources': [
-		'http://ftp.novell.com/pub/mono/sources/%{name}%{branch}/%{name}-%{version}.tar.bz2'
+		# 'http://ftp.novell.com/pub/mono/sources/%{name}%{branch}/%{name}-%{version}.tar.bz2'
+	],
+	'prep': [
+		svn_co_or_up,
+		'cp -a gtk-sharp-svn _build',
+		'cd _build/gtk-sharp-svn'
+	],
+	'build': [
+		'./bootstrap-for-the-insane --prefix=%{_prefix}',
+		'%{__make}'
+	],
+	'install': [
+		'%{__makeinstall}',
+		'find %{_prefix}/lib/mono/gac -iregex ".*/g[td]k-sharp.dll.config" -exec sed -ie "s/x11/quartz/g" "{}" \;'
 	]
 }
