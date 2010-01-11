559a000b9ac7aa70b8bdf9468a6d0269929ea5b4
Aaron Bockover <abockover@novell.com>
2010-01-11 13:23:49 -0500

    [build] do not include pango modules
    build pango with modules external, and apply the relative path
    patch from bgo#321419 so modules can be loaded in a relative
    way.

--- packages/pango.py
+++ packages/pango.py
@@ -1,14 +1,21 @@
-GnomePackage ('pango',
-	version_major = '1.26',
-	version_minor = '2',
-	configure_flags = [
-		'--without-x',
-		'--with-included-modules=basic-atsui'
-	],
-	override_properties = {
-		'make':
-			'( make -j%s -k -C modules || true ); '
-			'make -j%s' % \
-				(Package.profile.cpu_count, Package.profile.cpu_count)
-	}
-)
+class PangoPackage (GnomePackage):
+	def __init__ (self):
+		GnomePackage.__init__ (self,
+			'pango',
+			version_major = '1.26',
+			version_minor = '2',
+			configure_flags = [
+				'--without-x'
+			]
+		)
+
+		self.sources.extend ([
+			# patch from bgo#321419
+			'http://bugzilla-attachments.gnome.org/attachment.cgi?id=96023'
+		])
+
+	def prep (self):
+		GnomePackage.prep (self)
+		self.sh ('patch -p0 < "%{sources[1]}"')
+
+PangoPackage ()
