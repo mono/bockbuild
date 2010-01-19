08ad118950f7d671abb8628eaf2c4c41f328b668
Aaron Bockover <abockover@novell.com>
2010-01-19 13:16:50 -0500

    [osx] patch murrine for more osx-like rendering
    Do not render a highlight border on scroll bars
    and render solid arrows like OS X does

--- packages/murrine.py
+++ packages/murrine.py
@@ -1,3 +1,14 @@
-GnomePackage ('murrine',
-	version_major = '0.90',
-	version_minor = '3')
+class MurrinePackage (GnomePackage):
+	def __init__ (self):
+		GnomePackage.__init__ (self,
+			'murrine',
+			version_major = '0.90',
+			version_minor = '3')
+
+		self.sources.append ('patches/murrine-osx.patch')
+
+	def prep (self):
+		Package.prep (self)
+		self.sh ('patch -p1 < "%{sources[1]}"')
+
+MurrinePackage ()
