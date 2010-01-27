b84217a945125b13ae1c7539d3f49ca71607bfbc
Aaron Bockover <abockover@novell.com>
2010-01-27 14:52:48 -0500

    [bundle] Patches for small OSX improvements
    Pango patch to revert CoreText usage in ATSUI that caused glyph problems
    GTK patches to improve keybinding and single mouse button support

--- packages/gtk+.py
+++ packages/gtk+.py
@@ -14,7 +14,9 @@ class GtkPackage (GnomePackage):
 		if Package.profile.name == 'darwin':
 			self.gdk_target = 'quartz'
 			self.sources.extend ([
-				'http://github.com/jralls/gtk-osx-build/raw/master/patches/gdk-quartz-input-window.patch'
+				'http://github.com/jralls/gtk-osx-build/raw/master/patches/gdk-quartz-input-window.patch',
+				'http://git.dronelabs.com/gtk+/patch/?id=18773097865b173fb8c28b691e23d087f0269382',
+				'http://git.dronelabs.com/gtk+/patch/?id=729cbea7a2b27c4b8f2062316c0f406ab4c01dac'
 			])
 		elif Package.profile.name == 'linux':
 			self.gdk_target = 'x11'
@@ -22,6 +24,7 @@ class GtkPackage (GnomePackage):
 	def prep (self):
 		Package.prep (self)
 		if Package.profile.name == 'darwin':
-			self.sh ('patch -p1 < "%{sources[1]}"')
+			for p in range (2, len (self.sources)):
+				self.sh ('patch -p1 < "%{sources[' + str (p) + ']}"')
 
 GtkPackage ()
--- packages/pango.py
+++ packages/pango.py
@@ -14,8 +14,15 @@ class PangoPackage (GnomePackage):
 			'http://bugzilla-attachments.gnome.org/attachment.cgi?id=96023'
 		])
 
+		if Package.profile.name == 'darwin':
+			self.sources.extend ([
+				'http://git.gnome.org/browse/pango/patch/?id=0f06d7758bc37a4942342d2c17a88944cbc88adb',
+			])
+
 	def prep (self):
 		GnomePackage.prep (self)
 		self.sh ('patch -p0 < "%{sources[1]}"')
+		if Package.profile.name == 'darwin':
+			self.sh ('patch -p1 < "%{sources[2]}"')
 
 PangoPackage ()
