e2590e459edadafc5c3f852a620507fec6ae73cd
Aaron Bockover <abockover@novell.com>
2010-02-24 22:29:54 -0500

    [bundle] fix ige-mac-integration detection
    Included as a patch to the bundle since I missed this for the 1.5.4
    release. Yay bockbuild, yay!

--- packages/banshee.py
+++ packages/banshee.py
@@ -3,7 +3,8 @@ class BansheePackage (Package):
 		Package.__init__ (self, 'banshee-1', '1.5.4')
 
 		self.sources = [
-			'http://download.banshee-project.org/banshee/stable/%{version}/%{name}-%{version}.tar.bz2'
+			'http://download.banshee-project.org/banshee/stable/%{version}/%{name}-%{version}.tar.bz2',
+			'patches/banshee-1.5.4-ige-mac-integration.patch'
 		]
 
 		self.configure_flags = [
@@ -21,5 +22,9 @@ class BansheePackage (Package):
 				'--enable-osx',
 				'--with-vendor-build-id="banshee-project.org OSX 10.5+ i386/Intel"'
 			])
+
+	def prep (self):
+		Package.prep (self)
+		self.sh ('patch -p1 < "%{sources[1]}"')
 
 BansheePackage ()
