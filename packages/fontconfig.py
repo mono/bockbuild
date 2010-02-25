875947d87b608b99d8d4d75b212fbe3886f8e953
Aaron Bockover <abockover@novell.com>
2010-02-24 22:36:52 -0500

    [bundle] fix typo in fontconfig package

--- packages/fontconfig.py
+++ packages/fontconfig.py
@@ -1,5 +1,5 @@
 Package ('fontconfig', '2.7.3',
-	configure_flags = '--disable-docs',
+	configure_flags = [ '--disable-docs' ],
 	sources = [
 		'http://www.fontconfig.org/release/%{name}-%{version}.tar.gz'
 	]
