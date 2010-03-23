cea7e97b91e087e5eac94946a0a1d9e499466f8f
Aaron Bockover <abockover@novell.com>
2010-03-23 18:07:18 -0400

    [bundle] bump taglib-sharp to 2.0.3.7

--- packages/taglib-sharp.py
+++ packages/taglib-sharp.py
@@ -1,4 +1,4 @@
-Package ('taglib-sharp', '2.0.3.6',
+Package ('taglib-sharp', '2.0.3.7',
 	sources = [ 'http://download.banshee-project.org/%{name}/%{version}/%{name}-%{version}.tar.gz' ],
 	configure_flags = [ '--disable-docs' ]
 )
