3389c4fb2ec38fbb540963966618453566c899f0
Aaron Bockover <abockover@novell.com>
2010-03-26 12:26:53 -0400

    [monodevelop] stubbed initial build profile

---
+++ profiles/monodevelop/packages.py
@@ -0,0 +1,49 @@
+import os
+from bockbuild.darwinprofile import DarwinProfile
+
+class MonoDevelopPackages:
+	def __init__ (self):
+		# Toolchain
+		self.packages.extend ([
+			'autoconf.py',
+			'automake.py',
+			'libtool.py',
+			'gettext.py',
+			'pkg-config.py'
+		])
+
+		# Base Libraries
+		self.packages.extend ([
+			'libpng.py',
+			'libjpeg.py',
+			'libxml2.py',
+			'freetype.py',
+			'fontconfig.py',
+			'pixman.py',
+			'cairo.py',
+			'glib.py',
+			'pango.py',
+			'atk.py',
+			'intltool.py',
+			'gtk+.py',
+			'sqlite.py'
+		])
+
+		# Theme
+		self.packages.extend ([
+			'librsvg.py',
+			'icon-naming-utils.py',
+			'hicolor-icon-theme.py',
+			'tango-icon-theme.py',
+			'murrine.py'
+		])
+
+		# Mono
+		self.packages.extend ([
+			'mono.py',
+			'gtk-sharp.py',
+			'mono-addins.py',
+		])
+
+		self.packages = [os.path.join ('..', '..', 'packages', p)
+			for p in self.packages]
