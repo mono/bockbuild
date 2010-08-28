0d52fcddb938928763cdb02e6002d8f0516e214b
Aaron Bockover <abockover@novell.com>
2010-08-28 11:15:33 -0400

    Added enchant, cmake, libproxy-0.4
    Enchant is for WebKit (GTK, WIP)
    Cmake is now needed for libproxy, but it doesn't work yet

---
+++ packages/cmake.py
@@ -0,0 +1,3 @@
+Package ('cmake', '2.8.1', sources = [
+	'http://www.cmake.org/files/v2.8/%{name}-%{version}.tar.gz'
+])
---
+++ packages/enchant.py
@@ -0,0 +1,4 @@
+Package ('enchant', '1.6.0',
+	sources = [ 'http://www.abisource.com/downloads/%{name}/%{version}/%{name}-%{version}.tar.gz' ],
+	configure_flags = []
+)
