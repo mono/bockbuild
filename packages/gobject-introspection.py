45e3d80c4513358e80e48bfb8fc705f0a7e39be0
Aaron Bockover <aaron@abock.org>
2011-02-06 13:27:42 +0100

    Added gobject-introspection and libffi packages

---
+++ packages/gobject-introspection.py
@@ -0,0 +1,1 @@
+GnomePackage ('gobject-introspection', version_major = '0.10', version_minor = '1', configure_flags = ['--disable-tests'])
---
+++ packages/libffi.py
@@ -0,0 +1,3 @@
+Package ('libffi', '3.0.9', sources = [
+	'ftp://sources.redhat.com/pub/%{name}/%{name}-%{version}.tar.gz'
+])
