5ae98e6fec7b8e9a4bfeeaa8b1b1b5d630c5dc1d
Michael Hutchinson <m.j.hutchinson@gmail.com>
2011-07-21 21:02:46 +0100

    Profile for full deps for MD on Mac

---
+++ profiles/monodevelop-mac-dev/monodevelop-mac-dev.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python -B
+
+import os
+import sys
+
+sys.path.append ('../..')
+
+from bockbuild.darwinprofile import DarwinProfile
+from packages import MonoDevelopMacDevPackages
+
+class MonoDevelopMacDevProfile (DarwinProfile, MonoDevelopMacDevPackages):
+	def __init__ (self):
+		DarwinProfile.__init__ (self)
+		MonoDevelopMacDevPackages.__init__ (self)
+
+		self_dir = os.path.realpath (os.path.dirname (sys.argv[0]))
+
+MonoDevelopMacDevProfile ().build ()
+
+envscript = '''#!/bin/sh
+PROFNAME="monogtk"
+INSTALLDIR=%s/build-root/_install
+export DYLD_FALLBACK_LIBRARY_PATH="$INSTALLDIR/lib:/lib:/usr/lib:$DYLD_FALLBACK_LIBRARY_PATH"
+export C_INCLUDE_PATH="$INSTALLDIR/include:$C_INCLUDE_PATH"
+export ACLOCAL_PATH="$INSTALLDIR/share/aclocal:$ACLOCAL_PATH"
+export ACLOCAL_FLAGS="-I $INSTALLDIR/share/aclocal $ACLOCAL_FLAGS"
+export PKG_CONFIG_PATH="$INSTALLDIR/lib/pkgconfig:$INSTALLDIR/lib64/pkgconfig:$INSTALLDIR/share/pkgconfig:$PKG_CONFIG_PATH"
+#export CONFIG_SITE="$INSTALLDIR/$PROFNAME-config.site"
+export MONO_GAC_PREFIX="$INSTALLDIR:MONO_GAC_PREFIX"
+export MONO_ADDINS_REGISTRY="$INSTALLDIR/addinreg"
+export PATH="$INSTALLDIR/bin:$PATH"
+export MONO_INSTALL_PREFIX="$INSTALLDIR"
+
+#mkdir -p "$INSTALLDIR"
+#echo "test \"\$prefix\" = NONE && prefix=\"$INSTALLDIR\"" > $CONFIG_SITE
+
+PS1="[$PROFNAME] \w @ "
+''' % os.path.realpath (os.path.dirname (sys.argv[0]))
+
+with open('md-dev-env', 'w') as f:
+       f.write (envscript)
---
+++ profiles/monodevelop-mac-dev/packages.py
@@ -0,0 +1,53 @@
+import os
+from bockbuild.darwinprofile import DarwinProfile
+
+class MonoDevelopMacDevPackages:
+	def __init__ (self):
+		# Toolchain
+		self.packages.extend ([
+			'autoconf.py',
+			'automake.py',
+			'libtool.py',
+			'gettext.py',
+			'pkg-config.py'
+		])
+
+		# Base Libraries
+		self.packages.extend ([
+			'libpng.py',
+			'libjpeg.py',
+			'libxml2.py',
+			'freetype.py',
+			'fontconfig.py',
+			'pixman.py',
+			'cairo.py',
+			'glib.py',
+			#gtk-doc requres docbook, install it using brew
+			#'gtk-doc.py',
+			'pango.py',
+			'atk.py',
+			'intltool.py',
+			'gdk-pixbuf.py',
+			'gtk+.py',
+			'libglade.py',
+			'sqlite.py'
+		])
+
+		# Theme
+		self.packages.extend ([
+			'librsvg.py',
+			'icon-naming-utils.py',
+			'hicolor-icon-theme.py',
+			'tango-icon-theme.py',
+			'murrine.py'
+		])
+
+		# Mono
+		self.packages.extend ([
+			'mono.py',
+			'gtk-sharp.py',
+			'mono-addins.py',
+		])
+
+		self.packages = [os.path.join ('..', '..', 'packages', p)
+			for p in self.packages]
