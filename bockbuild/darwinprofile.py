092a46decab82d6c22d74e6ff714faacd0ee2bc1
Michael Hutchinson <m.j.hutchinson@gmail.com>
2011-10-25 23:49:10 +0100

    Use MacOS 10.7 SDK, gettext won't build w/ 10.6 SDK on Lion

--- bockbuild/darwinprofile.py
+++ bockbuild/darwinprofile.py
@@ -9,7 +9,7 @@ class DarwinProfile (UnixProfile):
 		UnixProfile.__init__ (self)
 		
 		self.name = 'darwin'
-		self.mac_sdk_path = '/Developer/SDKs/MacOSX10.6.sdk'
+		self.mac_sdk_path = '/Developer/SDKs/MacOSX10.7.sdk'
 		
 		if not os.path.isdir (self.mac_sdk_path):
 			raise IOError ('Mac OS X SDK does not exist: %s' \
@@ -19,7 +19,7 @@ class DarwinProfile (UnixProfile):
 		self.gcc_flags.extend ([
 			'-D_XOPEN_SOURCE',
 			'-isysroot %{mac_sdk_path}',
-			'-mmacosx-version-min=10.6',
+			'-mmacosx-version-min=10.7',
 		])
 		self.gcc_flags.extend (self.gcc_arch_flags)
 		self.ld_flags.extend (self.gcc_arch_flags)
