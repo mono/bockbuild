d7b1ee2d78c8097d6669ed8391341b2d0edd5f71
Michael Hutchinson <m.j.hutchinson@gmail.com>
2011-10-25 23:49:55 +0100

    Fix gettext build on Lion

--- packages/gettext.py
+++ packages/gettext.py
@@ -1,5 +1,34 @@
-GnuPackage ('gettext', '0.18.1.1', configure_flags = [
-	'--disable-java',
-	'--disable-libasprintf',
-	'--disable-openmp'
-])
+class GettextPackage (GnuPackage):
+	def __init__ (self):
+		GnuPackage.__init__ (self, 'gettext', '0.18.1.1',
+			configure_flags = [
+				'--disable-java',
+				'--disable-libasprintf',
+				'--disable-openmp'
+			]
+		)
+
+		if Package.profile.name == 'darwin':
+			self.configure_flags.extend ([
+				# only build the tools, osx has the lib
+				# https://github.com/mxcl/homebrew/blob/master/Library/Formula/gettext.rb
+				'--without-included-gettext',
+			])
+			self.sources.extend ([
+				# Fixes building on Lion
+				# http://lists.gnu.org/archive/html/bug-gnulib/2011-10/msg00018.html
+				# http://git.savannah.gnu.org/gitweb/?p=gnulib.git;a=patch;h=c5728261c324a75f8d23dd7d10cb42dde9420227
+				'patches/gettext-stpncpy.patch',
+				
+				# Don't build samples
+				# https://trac.macports.org/export/79183/trunk/dports/devel/gettext/files/patch-gettext-tools-Makefile.in
+				'patches/gettext-no-samples.patch',
+			])
+
+	def prep (self):
+		Package.prep (self)
+		if Package.profile.name == 'darwin':
+			for p in range (1, len (self.sources)):
+				self.sh ('patch -p1 < "%{sources[' + str (p) + ']}"')
+
+GettextPackage ()
