76e84a25c66e98b86883b5957275fd480096b8dd
Michael Hutchinson <m.j.hutchinson@gmail.com>
2011-10-25 23:50:34 +0100

    Fix Cairo 32-bit build on Lion

--- packages/cairo.py
+++ packages/cairo.py
@@ -1,6 +1,19 @@
 class CairoPackage (CairoGraphicsPackage):
 	def __init__ (self):
 		Package.__init__ (self, 'cairo', '1.10.2')
+
+		if Package.profile.name == 'darwin':
+			self.sources.extend ([
+				# Fixes building on Lion
+				# http://cgit.freedesktop.org/cairo/commit/?id=8664df767cb9dbe48647f9853e3dcf551701d3ca
+				'patches/cairo-lion.patch',
+			])
+
+	def prep (self):
+		Package.prep (self)
+		if Package.profile.name == 'darwin':
+			for p in range (1, len (self.sources)):
+				self.sh ('patch -p1 < "%{sources[' + str (p) + ']}"')
 	
 	def build (self):
 		self.configure_flags = [
