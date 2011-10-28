6b227f462596df88b0a4a3945426457e2b66f772
Michael Hutchinson <m.j.hutchinson@gmail.com>
2011-10-28 09:57:51 +0100

    Add post-2.24.7 commits from GTK 2.24 branch as patches

--- packages/gtk+.py
+++ packages/gtk+.py
@@ -13,8 +13,15 @@ class GtkPackage (GnomePackage):
 		if Package.profile.name == 'darwin':
 			self.gdk_target = 'quartz'
 			self.sources.extend ([
-				# add some missing keybindings
-#				'patches/gtk/gtkmackeys.patch',
+				# post-2.24.7 commits from 2.24 branch
+				# gtkquartz: don't free the string returned by get_bundle_path()
+				'patches/gtk/01-c040b03c2e32a773a4d9cf4019050c2f8a5b91ce.patch',
+				# gtkrc.key.mac: remove half the file, it was an accidential double paste
+				'patches/gtk/02-79a92f99a9dbdc7b1a651b0e8665807bd89c6632.patch',
+				# gtkrc.key.mac: add Command-cursor text navigation
+				'patches/gtk/03-ccf12f7b406ecbd8f0c26b0e6dc86d4593144dab.patch',
+				# gtkrc.key.mac: add '"' missing from last commit
+				'patches/gtk/04-e81b6971d85c7a782269454311b022ce14787486.patch',
 
 				# https://bugzilla.gnome.org/show_bug.cgi?id=516725
 				# http://bugzilla-attachments.gnome.org/attachment.cgi?id=197616
