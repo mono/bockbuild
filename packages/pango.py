33e17ebd7046e0968db2ee01f9e2759a382b62d9
Michael Hutchinson <m.j.hutchinson@gmail.com>
2011-11-15 13:58:48 -0500

    Update Pango to use CoreText

--- packages/pango.py
+++ packages/pango.py
@@ -2,7 +2,7 @@ class PangoPackage (GnomePackage):
 	def __init__ (self):
 		GnomePackage.__init__ (self,
 			'pango',
-			version_major = '1.28',
+			version_major = '1.29',
 			version_minor = '4',
 			configure_flags = [
 				'--without-x'
@@ -11,11 +11,21 @@ class PangoPackage (GnomePackage):
 
 		self.sources.extend ([
 			# patch from bgo#321419
-			'http://bugzilla-attachments.gnome.org/attachment.cgi?id=96023'
+			'http://bugzilla-attachments.gnome.org/attachment.cgi?id=96023',
+			# CoreText: stricter handling of FontSymbolic traits
+			'http://git.gnome.org/browse/pango/patch/?id=cce4c9f84350bb53371323ab96ccf9245e014f75',
+			# Get _pango_get_lc_ctype from system prefs on Mac OS X
+			'http://git.gnome.org/browse/pango/patch/?id=c21b1bfe1278de08673c495ba398fbdee874a778',
+			# Bug 647969 - CoreText backend needs proper font fallback/coverage support
+			# https://bugzilla.gnome.org/show_bug.cgi?id=647969
+			'http://bugzilla-attachments.gnome.org/attachment.cgi?id=201356',
 		])
 
 	def prep (self):
 		GnomePackage.prep (self)
 		self.sh ('patch -p0 < "%{sources[1]}"')
+		if Package.profile.name == 'darwin':
+			for p in range (2, len (self.sources)):
+				self.sh ('patch -p1 < "%{sources[' + str (p) + ']}"')
 
 PangoPackage ()
