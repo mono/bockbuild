06162f235253bc39ebf260b57eb434c984fd35a0
Duncan Mak <duncan.mak@xamarin.com>
2011-12-21 13:44:37 -0500

    Add a package definition for libgdiplus.

---
+++ packages/libgdiplus.py
@@ -0,0 +1,2 @@
+GitHubTarballPackage('mono', 'libgdiplus', '2.10', '5179dd3de1b06c6e4d3525dce890c25b4fa10975', configure = './autogen.sh --prefix="%{prefix}"')                                                                                                                                         
+
