dc4d36f6cc466d679c96496bd4d8117885458866
Michael Hutchinson <m.j.hutchinson@gmail.com>
2012-04-25 16:52:45 -0400

    [murrine] Update to 0.98.2

--- packages/murrine.py
+++ packages/murrine.py
@@ -1,18 +1,16 @@
-class MurrinePackage (GnomePackage):
+class MurrinePackage (GnomeXzPackage):
 	def __init__ (self):
 		GnomePackage.__init__ (self,
 			'murrine',
 			version_major = '0.98',
-			version_minor = '1')
+			version_minor = '2')
 
 		self.configure = 'autoreconf -fi && ./configure --prefix="%{prefix}"'
 
-		self.sources.append ('patches/murrine-link-pixman.patch')
 		# FIXME: this may need porting
 		# self.sources.append ('patches/murrine-osx.patch')
 
 	def prep (self):
 		Package.prep (self)
-		self.sh ('patch -p1 < "%{sources[1]}"')
 
 MurrinePackage ()
