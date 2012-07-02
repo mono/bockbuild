71b706f8476bb67884ad4c72c167a1a33e8d160f
Timo Dörr <timo@latecrew.de>
2012-07-02 20:25:12 +0200

    [libopentk, libsdl] package added
    Added libopentk, along with libsdl for Banshee OpenVP visualization
    plugin

---
+++ packages/libopentk.py
@@ -0,0 +1,5 @@
+Package ('opentk', '2010-10-06',
+	sources = [ 'http://downloads.sourceforge.net/project/opentk/opentk/opentk-1.0/2010-10-06/opentk-2010-10-06.zip' ],
+	source_dir_name = '%{name}',
+	configure_flags = [ '' ]
+)
---
+++ packages/libsdl.py
@@ -0,0 +1,4 @@
+Package ('SDL', '1.2.15',
+	sources = [ 'http://www.libsdl.org/release/SDL-1.2.15.tar.gz'],
+	configure_flags = [ '' ]
+)
