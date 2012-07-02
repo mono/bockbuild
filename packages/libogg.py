4abe45479bfef693665e12eab8978976e7a0a6c2
Timo Dörr <timo@latecrew.de>
2012-07-02 20:04:16 +0200

    [libogg, libvorbis] bump, fix for XCode 4.2.1

--- packages/libogg.py
+++ packages/libogg.py
@@ -1,1 +1,17 @@
-XiphPackage ('ogg', 'libogg', '1.2.2')
+class LiboggPackage (XiphPackage):
+        def __init__ (self):
+                XiphPackage.__init__ (self,
+                        project  = 'ogg',
+			name = 'libogg',
+                        version = '1.3.0')
+
+                self.configure = 'autoreconf -fi && ./configure --prefix="%{prefix}"'
+
+		# reduce optimization from -O4 to -O3 to allow compilation on Xcode 4.2.1
+                self.sources.append ('patches/libogg-opt.patch')
+
+        def prep (self):
+                Package.prep (self)
+                self.sh ('patch -p1 < "%{sources[1]}"')
+
+LiboggPackage ()
