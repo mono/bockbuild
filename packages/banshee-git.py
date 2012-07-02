f68e0a0e12784797e95b1533beeee113a33cf774
Timo Dörr <timo@latecrew.de>
2012-07-02 19:53:39 +0200

    [banshee] bump version, add banshee-git package
    Also added gtk-mac-integration.py package which is required by latest
    banshee-git

--- packages/banshee-git.py
+++ packages/banshee-git.py
@@ -1,21 +1,18 @@
 class BansheePackage (Package):
 	def __init__ (self):
-		Package.__init__ (self, 'banshee', 'stable-2.4')
+		Package.__init__ (self, 'banshee', 'master')
 
 		self.sources = [
 			'git://git.gnome.org/banshee'
 		]
-		self.git_branch = 'stable-2.4'
+		self.git_branch = 'master'
 
 		self.configure = [ 'NOCONFIGURE=1 ./autogen.sh && ./profile-configure %{profile.name} --prefix=%{prefix}' ]
 		self.sources.extend([
-
-			# switch over from ige_* to gtk_* binding
-			'patches/banshee-gtk-mac-integration.patch'
 		])
 
 	def prep (self):
 		Package.prep (self)
-		self.sh ('patch -p1 < %{sources[1]}')
+		#self.sh ('patch -p1 < %{sources[1]}')
 
 BansheePackage ()
---
+++ packages/gtk-mac-integration.py
@@ -0,0 +1,23 @@
+class Gtkmacintegration (GnomeXzPackage):
+	def __init__ (self):
+		GnomeXzPackage.__init__ (self, 'gtk-mac-integration',
+		version_major = '1.0',
+		version_minor = '1',
+		sources = [ 'ftp://ftp.gnome.org/pub/gnome/sources/gtk-mac-integration/1.0/%{name}-%{version}.tar.xz' ],
+		configure_flags = [
+			'--prefix="%{prefix}"'
+		])
+
+		self.sources.extend([
+			# fix Alt/Mod1 key behaviour on OS X - required
+			# for key accelerators using the alt/option key on OSX 
+			'http://git.gnome.org/browse/gtk-mac-integration/patch/?id=19cf4ee74821aa5d6d70d0a069178fa5a684ff7f'
+		])
+
+	def prep (self):
+		Package.prep (self)
+		if Package.profile.name == 'darwin':
+			for p in range (1, len (self.sources)):
+				self.sh ('patch -p1 < "%{sources[' + str (p) + ']}"')
+
+Gtkmacintegration()
