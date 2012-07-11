8a0e688b2ab59b8a08236a35c1921365ef21b936
Michael Hutchinson <m.j.hutchinson@gmail.com>
2012-07-11 19:44:43 -0400

    [libpng] Upgrade to 1.4.12

--- packages/libpng.py
+++ packages/libpng.py
@@ -1,7 +1,7 @@
 class LibPngPackage (Package):
 	def __init__ (self):
-		Package.__init__(self, 'libpng', '1.4.4', 
-										 sources = ['http://downloads.sourceforge.net/sourceforge/libpng/libpng-1.4.4.tar.gz'],
+		Package.__init__(self, 'libpng', '1.4.12', 
+										 sources = ['http://downloads.sourceforge.net/sourceforge/libpng/libpng-1.4.12.tar.xz'],
 										 configure_flags = ['--enable-shared'])
 
 LibPngPackage()
