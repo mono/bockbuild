ca292082d086a501f4b7357cb0935e17924733ca
Duncan Mak <duncan.mak@xamarin.com>
2012-08-27 19:02:09 -0400

    Revert "Use glibtool." because the proper fix landed in xamarin-gtk-theme.
    This reverts commit 2226659e1ca8ed8c5907b822965732f225413405.

--- packages/xamarin-gtk-theme.py
+++ packages/xamarin-gtk-theme.py
@@ -1,6 +1,6 @@
 Package ('xamarin-gtk-engine', 'master',
 	sources = [ 'git@github.com:lanedo/xamarin-gtk-theme.git' ],
 	override_properties = { 'configure':
-		'LIBTOOL=glibtool ./autogen.sh --prefix=%{prefix}'
+		'./autogen.sh --prefix=%{prefix}'
 	}
 )
