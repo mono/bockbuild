e9ee7c4ec1047e05349f67d492bad181e0753b84
Michael Hutchinson <m.j.hutchinson@gmail.com>
2012-09-30 17:31:30 -0400

    [gtk-sharp] Upgrade to 2.12.12

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -1,14 +1,11 @@
 class GtkSharpPackage (Package):
 	def __init__ (self):
 		Package.__init__ (self, 'gtk-sharp', '2-12-branch')
-		self.commit = 'b078aacaf263af84605812d778b62afbdf3e1b59'
+		self.commit = 'febd3819c52a51d247c67d156ee3fe68d730bc54'
 		self.source_dir_name = 'mono-gtk-sharp-%s' % self.commit[:7]
 		self.configure = './bootstrap-2.12 --prefix="%{prefix}"'
 		self.sources = [
 			'http://github.com/mono/gtk-sharp/tarball/%{commit}',
-
-			#container leak fix
-			'https://github.com/mono/gtk-sharp/pull/50.patch'
 		]
 
 	def prep (self):
