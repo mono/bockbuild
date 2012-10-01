e7deeeb8b79f701541c1dd23e44bdc388367aabc
Alex Corrado <alexc@xamarin.com>
2012-10-01 14:18:47 -0400

    Update qtk-quartz-engine clone URL because I changed my github username

--- packages/gtk-quartz-engine.py
+++ packages/gtk-quartz-engine.py
@@ -1,5 +1,5 @@
 Package ('gtk-quartz-engine', 'master',
-	sources = [ 'git://github.com/nirvanai/gtk-quartz-engine.git' ],
+	sources = [ 'git://github.com/chkn/gtk-quartz-engine.git' ],
 	override_properties = { 'configure':
 		'libtoolize --force --copy && '
 		'aclocal && '
