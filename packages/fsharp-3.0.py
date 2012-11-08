ce8bb976d113da8fbc423437d065d34199dbe57b
Duncan Mak <duncan.mak@xamarin.com>
2012-11-08 17:14:53 -0500

    Use the full commit hash so that we can download from github.com.

--- packages/fsharp-3.0.py
+++ packages/fsharp-3.0.py
@@ -4,7 +4,7 @@ class FsharpPackage(GitHubTarballPackage):
 		GitHubTarballPackage.__init__(self,
 			'fsharp', 'fsharp',
 			'3.0.13',
-			'3.0.13',
+			'893b8a3a9326b0a2008b5f7322dd8002b8065a69',
 			configure = '')
 
 	def build(self):
