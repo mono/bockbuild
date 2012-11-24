c46f075d25025da62d3ddd7316d2ce99c09cb4c2
Duncan Mak <duncan@a-chinaman.com>
2012-11-23 20:06:57 -0500

    Update F# 3.0 to point to 3.0.22

--- packages/fsharp-3.0.py
+++ packages/fsharp-3.0.py
@@ -3,8 +3,8 @@ class FsharpPackage(GitHubTarballPackage):
 	def __init__(self):
 		GitHubTarballPackage.__init__(self,
 			'fsharp', 'fsharp',
-			'3.0.15',
-			'e8428a2ec77a6605aa545ade2b2ca93c5f64ae51',
+			'3.0.22',
+			'4ea45bbc75eda522d43ed50ece1f9b8103697116',
 			configure = '')
 
 	def build(self):
