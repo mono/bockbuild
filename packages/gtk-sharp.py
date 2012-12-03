f329815b1dff3f25f1bdbae8ad6316992f3e875a
Alan McGovern <alan.mcgovern@gmail.com>
2012-12-03 15:11:20 +0000

    Bump the gtk-sharp commit
    Pulls in fixes for broken delegates, p/invokes and leaks in Gtk.Container
    Conflicts:
    	packages/gtk-sharp.py

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -1,7 +1,7 @@
 class GtkSharpPackage (Package):
 	def __init__ (self):
 		Package.__init__ (self, 'gtk-sharp', '2-12-branch')
-		self.commit = 'b078aacaf263af84605812d778b62afbdf3e1b59'
+		self.commit = 'acbb04600d2dbdbe889117c07ee660818abd7070'
 		self.source_dir_name = 'mono-gtk-sharp-%s' % self.commit[:7]
 		self.configure = './bootstrap-2.12 --prefix="%{prefix}"'
 		self.sources = [
