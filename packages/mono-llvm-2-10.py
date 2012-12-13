e4275c4f1a408dd9f8095bef4ed650ccc54401e9
Alan McGovern <alan.mcgovern@gmail.com>
2012-12-13 12:21:59 +0000

    Fix llvm so it doesn't corrupt the env when configuring itself

--- packages/mono-llvm-2-10.py
+++ packages/mono-llvm-2-10.py
@@ -1,4 +1,4 @@
 GitHubTarballPackage ('mono', 'llvm', '2.10', '943edbc1a93df204d687d82d34d2b2bdf9978f4e',
-	configure = 'CFLAGS="-m32" CPPFLAGS="-m32" CXXFLAGS="-m32" LDFLAGS="-m32" ./configure --prefix="%{prefix}" --enable-optimized --enable-targets="x86 x86_64" --target=i386-apple-darwin10.8.0',
+	configure = './configure --prefix="%{prefix}" --enable-optimized --enable-targets="x86 x86_64" --target=i386-apple-darwin10.8.0',
 	override_properties = { 'make': 'make' }
 )
