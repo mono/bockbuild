d89b4b1b990c0bd91039f441c35f783b4ac214ee
Alan McGovern <alan@xamarin.com>
2012-12-13 14:23:37 +0000

    [libtool] Symlink libtool* -> glibtool*
    This ensures that we will always use the libtool built by bockbuild
    no matter what package we are building. We cannot rely on system
    glibtool as it is broken on macos.

--- packages/libtool.py
+++ packages/libtool.py
@@ -1,1 +1,13 @@
-GnuPackage ('libtool', '2.4.2')
+class LibtoolPackage (GnuPackage):
+        def __init__ (self):
+                GnuPackage.__init__ (self, 'libtool', '2.4.2')
+
+        def install (self):
+		Package.install (self)
+		self.sh ('rm -f "%{prefix}/bin/glibtool"')
+                self.sh ('ln -s "%{prefix}/bin/libtool"  "%{prefix}/bin/glibtool"')
+                self.sh ('rm -f "%{prefix}/bin/glibtoolize"')
+                self.sh ('ln -s "%{prefix}/bin/libtoolize"  "%{prefix}/bin/glibtoolize"')
+
+
+LibtoolPackage ()
