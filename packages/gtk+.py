5190c2c846c76764073215f11d5f7f7dc99c4676
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-01-04 01:01:49 +0000

    [gtk] Added some commits from git

--- packages/gtk+.py
+++ packages/gtk+.py
@@ -19,12 +19,24 @@ class GtkPackage (GnomeXzPackage):
 				'patches/gtkrc',
 
 				#post 2.24.14 patches
-				# gtk: really really fix the crash in the clipboard fix
-				'patches/fix_clipboard_crash_on_invoke_null_func.patch',
 				# quartz: fix crash in the recent clipboard "fix", and really fix it
 				'http://git.gnome.org/browse/gtk+/patch/?id=4a8df7a33c298d22bf78b947d0e861fc03ec70e1',
 				# gtk: fix scrolling in modal dialogs when event_widget is insensitive
 				'http://git.gnome.org/browse/gtk+/patch/?id=31ae1a0b5bb257c86cc2393e065ded8506b4ef4a',
+				# quartz: make setting_same_owner member of GtkClipboardOwner @public
+				'http://git.gnome.org/browse/gtk+/patch/?id=e3e055f8551ac8ee033f361261c849c612554184',
+				# quartz: don't call a NULL get_func() in gtk_clipboard_store()
+				'http://git.gnome.org/browse/gtk+/patch/?id=f1c105b94fc3c3572a234c93c47252a3ff82218b',
+				# Move single-include guards inside include guards
+				'http://git.gnome.org/browse/gtk+/patch/?id=1f0f3994694ec3f1baa43932c186db78814b2abd',
+				# quartz: ensure window being (un)fullscreened is visible
+				'http://git.gnome.org/browse/gtk+/patch/?id=62f1d871b70a8e08b899942827386d6f3222c986',
+				# quartz: make sure all old properties are set on the new toplevel
+				'http://git.gnome.org/browse/gtk+/patch/?id=a8008b796f14444dff3ac46af884238fc4f214f6',
+				# quartz: Make sure the old toplevel is closed on recreation
+				'http://git.gnome.org/browse/gtk+/patch/?id=30deba453a045107eadd4deea572e29192c298c1',
+				# quartz: retain content view when switching over toplevel
+				'http://git.gnome.org/browse/gtk+/patch/?id=184407309f83a06b9215c8123091263d483edc8b',
 
 				# smooth scrolling, scrollbars, overscroll
 				'patches/gtk-scrolling/0001-Add-gdk_screen_get_monitor_workarea-and-use-it-all-o.patch',
