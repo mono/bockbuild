e74890f037b4f5935725ec94395d11ac75ef5236
Alan McGovern <alan@xamarin.com>
2013-01-15 15:25:35 +0000

    Really really *really* fix the clipboard crasher in gtk/mac

--- packages/gtk+.py
+++ packages/gtk+.py
@@ -25,6 +25,8 @@ class GtkPackage (GnomeXzPackage):
 				'http://git.gnome.org/browse/gtk+/patch/?id=31ae1a0b5bb257c86cc2393e065ded8506b4ef4a',
 				# quartz: make setting_same_owner member of GtkClipboardOwner @public
 				'http://git.gnome.org/browse/gtk+/patch/?id=e3e055f8551ac8ee033f361261c849c612554184',
+				# quartz: really don't call a NULL function in gtk_clipboard_store()
+				'http://git.gnome.org/browse/gtk+/patch/?id=bc3f1893aa26761c0009ddc993b48623bcfbe4ed',
 				# quartz: don't call a NULL get_func() in gtk_clipboard_store()
 				'http://git.gnome.org/browse/gtk+/patch/?id=f1c105b94fc3c3572a234c93c47252a3ff82218b',
 				# Move single-include guards inside include guards
