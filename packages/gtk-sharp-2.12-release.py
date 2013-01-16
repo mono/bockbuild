c6c3e7f78989066816f67bab9879b5cf391f6275
Alan McGovern <alan@xamarin.com>
2013-01-16 20:53:57 +0000

    Bump gtk-sharp version and build from a new tarball

--- packages/gtk-sharp-2.12-release.py
+++ packages/gtk-sharp-2.12-release.py
@@ -1,8 +1,8 @@
 class GtkSharp212ReleasePackage (Package):
 	def __init__ (self):
-		Package.__init__ (self, 'gtk-sharp', '2.12.11')
+		Package.__init__ (self, 'gtk-sharp', '2.12.12')
 		# self.configure = './bootstrap-2.12 --prefix="%{prefix}"'
-		self.sources = ['http://download.mono-project.com/sources/gtk-sharp212/gtk-sharp-2.12.11.tar.bz2']
+		self.sources = ['http://files.xamarin.com/~alan/gtk-sharp-2.12.12.tar.gz']
 		self.make = 'make CSC=gmcs'
 
 GtkSharp212ReleasePackage ()
