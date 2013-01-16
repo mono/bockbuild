e3012ea761b74d0b2108e3b6db6353c87e81959a
Alan McGovern <alan@xamarin.com>
2013-01-16 20:53:06 +0000

    Revert "[mono] Bundle tip of the gtk-sharp-2-12 branch"
    This reverts commit 257f3437a8810d7cd6488b779fe18f9f6beca95a.

--- profiles/mono-2-10/packages.py
+++ profiles/mono-2-10/packages.py
@@ -56,7 +56,7 @@ class MonoReleasePackages:
 				'mono-2-10.py',
 				'libgdiplus-2-10.py',
 				'xsp.py',
-				'gtk-sharp.py',
+				'gtk-sharp-2.12-release.py',
 				'boo.py',
 				# 'nant.py',
 				'ironlangs.py',
