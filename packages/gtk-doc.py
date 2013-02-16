bf7626df74d78f2811f20173fb21c36a96cc9500
Duncan Mak <duncan.mak@xamarin.com>
2013-02-15 21:08:36 -0500

    Set xml catalog to the right prefix.

--- packages/gtk-doc.py
+++ packages/gtk-doc.py
@@ -1,3 +1,3 @@
-GnomePackage ('gtk-doc', version_major = '1', version_minor = '17', sources = [
+GnomePackage ('gtk-doc', version_major = '1', version_minor = '17', configure_flags = ['--with-xml-catalog="%{prefix}/etc/xml/catalog"'], sources = [
         'http://ftp.gnome.org/pub/gnome/sources/%{name}/%{version}/%{name}-%{version}.tar.bz2'
 	])
