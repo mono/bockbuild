e310f059d54e3cc02e1a6534d8c75a5a5b5121ea
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-02-25 18:43:55 -0500

    [gtk-sharp] Fix build not to rely on github tarballs

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -1,16 +1,6 @@
-class GtkSharpPackage (Package):
-	def __init__ (self):
-		Package.__init__ (self, 'gtk-sharp', '2-12-branch')
-		self.commit = 'acbb04600d2dbdbe889117c07ee660818abd7070'
-		self.source_dir_name = 'mono-gtk-sharp-%s' % self.commit[:7]
-		self.configure = './bootstrap-2.12 --prefix="%{prefix}"'
-		self.sources = [
-			'http://github.com/mono/gtk-sharp/tarball/%{commit}',
-		]
-
-	def prep (self):
-		Package.prep (self)
-		for p in range (1, len (self.sources)):
-			self.sh ('patch -p1 < "%{sources[' + str (p) + ']}"')
-
-GtkSharpPackage ()
+Package ('gtk-sharp', '2-12-branch',
+	sources = [ 'git@github.com:mono/gtk-sharp.git' ],
+	revision = 'acbb04600d2dbdbe889117c07ee660818abd7070',
+	override_properties = { 'configure':
+		'./bootstrap-2.12 --prefix="%{prefix}"'
+	})
