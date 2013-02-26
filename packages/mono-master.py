ec0fd05a6ab97d4910c458c69f608181b01da8a5
Duncan Mak <duncan.mak@xamarin.com>
2013-02-26 13:21:06 -0500

    Bump version number to 3.0.6

--- packages/mono-master.py
+++ packages/mono-master.py
@@ -3,7 +3,7 @@ import os
 class MonoMasterPackage(Package):
 
 	def __init__(self):
-		Package.__init__(self, 'mono', '3.0.5',
+		Package.__init__(self, 'mono', '3.0.6',
 			sources = ['git://github.com/mono/mono'],
 			revision = os.getenv('MONO_BUILD_REVISION'),
 			configure_flags = [
