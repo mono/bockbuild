295804095dd307ed6e8481e45422a1f8f21c3591
Jeffrey Stedfast <jeff@xamarin.com>
2013-03-07 13:55:06 -0500

    [gtk-sharp] bumped gtk-sharp revision for a null Gdk.Event fix

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -1,6 +1,6 @@
 Package ('gtk-sharp', '2-12-branch',
 	sources = [ 'git@github.com:mono/gtk-sharp.git' ],
-	revision = 'acbb04600d2dbdbe889117c07ee660818abd7070',
+	revision = '36e3f5b803ec8c7761910a278669594c76582b8b',
 	override_properties = { 'configure':
 		'./bootstrap-2.12 --prefix="%{prefix}"'
 	})
