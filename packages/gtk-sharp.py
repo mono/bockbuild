35d4020112b96ad799ce0be069cb5f14df03e3e2
Jeffrey Stedfast <jeff@xamarin.com>
2013-03-07 14:57:13 -0500

    [gtk-sharp] bumped for another fix

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -1,6 +1,6 @@
 Package ('gtk-sharp', '2-12-branch',
 	sources = [ 'git@github.com:mono/gtk-sharp.git' ],
-	revision = '36e3f5b803ec8c7761910a278669594c76582b8b',
+	revision = '98e9439afa4b4da7cd2f3d1ab92b6415ab50b01d',
 	override_properties = { 'configure':
 		'./bootstrap-2.12 --prefix="%{prefix}"'
 	})
