4a332d4b94bdc4daad886d6a1445113ea5040e31
Duncan Mak <duncan.mak@xamarin.com>
2013-03-08 17:59:57 -0500

    Switch freedesktop.org pkgs to use our own server.

--- packages/pkg-config.py
+++ packages/pkg-config.py
@@ -1,1 +1,11 @@
-FreeDesktopPackage ('%{name}', 'pkg-config', '0.25')
+class PkgConfigPackage (Package):
+	def __init__ (self):
+		Package.__init__(self,
+										 name = 'pkg-config',
+										 version = '0.25',
+										 sources = ['http://storage.bos.xamarin.com/bockbuild-sources/pkg-config-0.25.tar.gz'])
+
+PkgConfigPackage ()
+
+
+
