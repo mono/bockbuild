d4469b0ad3c42f06c1c287ec9c463dbfb9d2fa1a
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-03-27 16:18:47 -0400

    [pango] Update to 1.34.0 to reduce patchset

--- packages/pango.py
+++ packages/pango.py
@@ -2,8 +2,8 @@ class PangoPackage (GnomeXzPackage):
 	def __init__ (self):
 		GnomePackage.__init__ (self,
 			'pango',
-			version_major = '1.30',
-			version_minor = '1',
+			version_major = '1.34',
+			version_minor = '0',
 			configure_flags = [
 				'--without-x'
 			]
@@ -13,27 +13,14 @@ class PangoPackage (GnomeXzPackage):
 			# 1
 			# Bug 321419 - Allow environment var substitution in Pango config
 			# https://bugzilla.gnome.org/show_bug.cgi?id=321419
-			'http://bugzilla-attachments.gnome.org/attachment.cgi?id=96023',
-			
-			# 2
-			# revert git 0e091322, it requires unstable glib simply for some deprecation marker macros
-			'patches/pango-stable-glib.patch',
+			'patches/pango-relative-config-file.patch',
 
-			# Post-1.30.1 commits from git
-
-			# coretext: don't insert item in the hash if it originated from the hash
-			'http://git.gnome.org/browse/pango/patch/?id=70a85d441d973883af4afb57599bc570eeea4c83',
-
-			# coretext: silently fail on invalid UTF8 instead of crashing
-			'http://git.gnome.org/browse/pango/patch/?id=fa4cd1a232529898a616e86ab89965d5ffc52347',
 		])
 
 	def prep (self):
 		GnomePackage.prep (self)
-		self.sh ('patch -p0 < "%{sources[1]}"')
-		self.sh ('patch -p1 < "%{sources[2]}"')
 		if Package.profile.name == 'darwin':
-			for p in range (3, len (self.sources)):
+			for p in range (1, len (self.sources)):
 				self.sh ('patch -p1 < "%{sources[' + str (p) + ']}"')
 
 PangoPackage ()
