ad87b415af1ecc925661acee92ac43aae24e4330
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-03-27 16:14:42 -0400

    [glib] Update to 2.36 now it supports atomics on Mac again

--- packages/glib.py
+++ packages/glib.py
@@ -2,21 +2,21 @@ class GlibPackage (GnomeXzPackage):
 	def __init__ (self):
 		GnomePackage.__init__ (self,
 			'glib',
-			version_major = '2.30',
-			version_minor = '3')
+			version_major = '2.36',
+			version_minor = '0')
 
 		self.darwin = Package.profile.name == 'darwin'
 
 		if Package.profile.name == 'darwin':
 			#link to specific revisions for glib 2.30.x
 			self.sources.extend ([
-				'https://trac.macports.org/export/62644/trunk/dports/devel/glib2/files/config.h.ed',
-				'https://trac.macports.org/export/87503/trunk/dports/devel/glib2/files/patch-configure.diff',
-				'https://trac.macports.org/export/92347/trunk/dports/devel/glib2/files/patch-glib_gunicollate.c.diff',
-				'https://trac.macports.org/export/92347/trunk/dports/devel/glib2/files/patch-gi18n.h.diff',
-				'https://trac.macports.org/export/92347/trunk/dports/devel/glib2/files/patch-gio_xdgmime_xdgmime.c.diff',
-				'https://trac.macports.org/export/87469/trunk/dports/devel/glib2/files/patch-glib-2.0.pc.in.diff',
-				'https://trac.macports.org/export/87469/trunk/dports/devel/glib2/files/patch-gio_gdbusprivate.c.diff',
+				'https://trac.macports.org/export/91680/trunk/dports/devel/glib2/files/config.h.ed',
+				'https://trac.macports.org/export/98985/trunk/dports/devel/glib2/files/patch-configure.diff',
+				'https://trac.macports.org/export/42728/trunk/dports/devel/glib2/files/patch-gi18n.h.diff',
+				'https://trac.macports.org/export/92608/trunk/dports/devel/glib2/files/patch-gio_gdbusprivate.c.diff',
+				'https://trac.macports.org/export/49466/trunk/dports/devel/glib2/files/patch-gio_xdgmime_xdgmime.c.diff',
+				'https://trac.macports.org/export/91680/trunk/dports/devel/glib2/files/patch-glib-2.0.pc.in.diff',
+				'https://trac.macports.org/export/64476/trunk/dports/devel/glib2/files/patch-glib_gunicollate.c.diff',
 			])
 
 	def prep (self):
