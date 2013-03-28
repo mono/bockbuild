f02848c93bb1225b6d1d981eea9e7fedb45c5639
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-03-27 20:29:20 -0400

    Use specific revisions of theme engines

--- packages/gtk-quartz-engine.py
+++ packages/gtk-quartz-engine.py
@@ -1,6 +1,7 @@
 Package ('gtk-quartz-engine', 'master',
-	sources = [ 'git://github.com/chkn/gtk-quartz-engine.git' ],
+	sources = [ 'git://github.com/mono/gtk-quartz-engine.git' ],
 	override_properties = {
 		'configure': './autogen.sh --prefix=%{prefix}'
-	}
+	},
+	revision = '2bc5380f27d5b41d54bd91638f6443f0bc2dda69'
 )
--- packages/xamarin-gtk-theme.py
+++ packages/xamarin-gtk-theme.py
@@ -2,5 +2,6 @@ Package ('xamarin-gtk-engine', 'master',
 	sources = [ 'git@github.com:mono/xamarin-gtk-theme.git' ],
 	override_properties = { 'configure':
 		'./autogen.sh --prefix=%{prefix}'
-	}
+	},
+	revision = '01d77e8b0051b899837934c454d3af0d4f197b89',
 )
