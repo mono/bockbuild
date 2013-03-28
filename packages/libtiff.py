04913a0658a6b205304b9714cf4f1ff0ffbe246f
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-03-28 17:56:37 -0400

    [libtiff] Patch to build on Snow Leopard

--- packages/libtiff.py
+++ packages/libtiff.py
@@ -1,3 +1,23 @@
-Package ('tiff', '4.0.3', sources = [
-	'http://download.osgeo.org/libtiff/tiff-%{version}.tar.gz'
-])
+class LibTiffPackage (Package):
+	def __init__ (self):
+		Package.__init__ (self, 'tiff', '4.0.3',
+			configure_flags = [
+			],
+			sources = [
+				'http://download.osgeo.org/libtiff/tiff-%{version}.tar.gz',
+			])
+
+		if Package.profile.name == 'darwin':
+			self.sources.extend ([
+				# Fix Snow Leopard build
+				# http://jira.freeswitch.org/secure/attachment/17487/tiff-4.0.2-macosx-2.patch
+				'patches/tiff-4.0.2-macosx-2.patch'
+			])
+
+	def prep (self):
+		Package.prep (self)
+		if Package.profile.name == 'darwin':
+			for p in range (1, len (self.sources)):
+				self.sh ('patch -p1 < "%{sources[' + str (p) + ']}"')
+
+LibTiffPackage ()
