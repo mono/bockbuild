a1bb6b9d4b20c213fd478f85c9e437abf2e347b2
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-03-28 13:51:48 -0400

    Revert "Switch freedesktop.org pkgs to use our own server."
    This reverts commit 4a332d4b94bdc4daad886d6a1445113ea5040e31.
    
    Let's not start running public mirrors of individual FDO packages.
    That way lies madness.

--- packages/hicolor-icon-theme.py
+++ packages/hicolor-icon-theme.py
@@ -1,10 +1,1 @@
-class HiColorIconThemePackage (Package):
-	def __init__ (self):
-		Package.__init__(self,
-										 name = 'hicolor-icon-theme',
-										 version = '0.12',
-										 sources = ['http://storage.bos.xamarin.com/bockbuild-sources/hicolor-icon-theme-0.12.tar.gz'])
-
-HiColorIconThemePackage ()
-
-
+FreeDesktopPackage ('icon-theme', 'hicolor-icon-theme', '0.12')
--- packages/pkg-config.py
+++ packages/pkg-config.py
@@ -1,11 +1,1 @@
-class PkgConfigPackage (Package):
-	def __init__ (self):
-		Package.__init__(self,
-										 name = 'pkg-config',
-										 version = '0.25',
-										 sources = ['http://storage.bos.xamarin.com/bockbuild-sources/pkg-config-0.25.tar.gz'])
-
-PkgConfigPackage ()
-
-
-
+FreeDesktopPackage ('%{name}', 'pkg-config', '0.25')
