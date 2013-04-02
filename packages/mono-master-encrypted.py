4833464b1e3bbc0929a1c138c94c070c8182b4fa
Duncan Mak <duncan.mak@xamarin.com>
2013-04-02 15:42:10 -0400

    Ignore the return code from quilt pop.

--- packages/mono-master-encrypted.py
+++ packages/mono-master-encrypted.py
@@ -45,22 +45,8 @@ class MonoMasterEncryptedPackage(Package):
 
 		# Use quilt to apply the patch queue
 		self.cd (build_root)
-
-		# mono = os.path.join (build_root, "mono")
-		# mono_extensions = os.path.join (build_root, "mono-extensions")
-		# full_mono = os.path.join (build_root, "%s-%s" % (self.name, self.version))
-		# full_mono_extensions = os.path.join (build_root, "%s-%s-%s" % (self.name, self.version, "mono-extensions"))
-
-		# if not (os.path.exists (mono) and os.path.join (os.path.dirname (mono), os.readlink (mono)) == full_mono):
-		# 	if os.path.exists(mono): os.remove (mono)
-		# 	os.symlink (full_mono, mono)
-
-		# if not (os.path.exists (mono_extensions) and os.path.join (os.path.dirname (mono_extensions), os.readlink (mono_extensions)) == full_mono_extensions):
-		# 	if os.path.exists(mono_extensions): os.remove (mono_extensions)
-		# 	os.symlink (full_mono_extensions, mono-extensions)
-
 		self.sh ("export QUILT_PATCHES=%s-%s-mono-extensions" % (self.name, self.version))
-		self.sh ('/usr/local/bin/quilt pop -af')
+		self.sh ('/usr/local/bin/quilt pop -af || true') # ignore its return code
 		self.sh ('/usr/local/bin/quilt push -a')
 
 	def prep (self):
