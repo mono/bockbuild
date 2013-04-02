608de5722a8d63baa8f78dfa76b838f1ed863c64
Duncan Mak <duncan.mak@xamarin.com>
2013-04-02 15:23:39 -0400

    Take out this symlink bumbo-jumbo.

--- packages/mono-master-encrypted.py
+++ packages/mono-master-encrypted.py
@@ -43,26 +43,30 @@ class MonoMasterEncryptedPackage(Package):
 		else:
 			self.sh ('git clone --local --shared "%s" "%s"' % (extension, dirname))
 
-		try:
-			# Use quilt to apply the patch queue
-			self.cd (build_root)
-			mono = os.path.join (build_root, "mono")
-			mono_extensions = os.path.join (build_root, "mono-extensions")
-			if os.path.exists(mono): os.remove (mono)
-			if os.path.exists(mono_extensions): os.remove (mono_extensions)
-			os.symlink (os.path.join (build_root, "%s-%s" % (self.name, self.version)), mono)
-			os.symlink (os.path.join (build_root, "%s-%s-%s" % (self.name, self.version, "mono-extensions")), mono-extensions)
-			self.sh ('export QUILT_PATCHES=mono-extensions')
-			self.sh ('/usr/local/bin/quilt pop -af')
-			self.sh ('/usr/local/bin/quilt push -a')
-		finally:
-			os.remove ('mono')
-			os.remove ('mono-extensions')
+		# Use quilt to apply the patch queue
+		self.cd (build_root)
+
+		# mono = os.path.join (build_root, "mono")
+		# mono_extensions = os.path.join (build_root, "mono-extensions")
+		# full_mono = os.path.join (build_root, "%s-%s" % (self.name, self.version))
+		# full_mono_extensions = os.path.join (build_root, "%s-%s-%s" % (self.name, self.version, "mono-extensions"))
+
+		# if not (os.path.exists (mono) and os.path.join (os.path.dirname (mono), os.readlink (mono)) == full_mono):
+		# 	if os.path.exists(mono): os.remove (mono)
+		# 	os.symlink (full_mono, mono)
+
+		# if not (os.path.exists (mono_extensions) and os.path.join (os.path.dirname (mono_extensions), os.readlink (mono_extensions)) == full_mono_extensions):
+		# 	if os.path.exists(mono_extensions): os.remove (mono_extensions)
+		# 	os.symlink (full_mono_extensions, mono-extensions)
+
+		self.sh ("export QUILT_PATCHES=%s-%s-mono-extensions" % (self.name, self.version))
+		self.sh ('/usr/local/bin/quilt pop -af')
+		self.sh ('/usr/local/bin/quilt push -a')
 
 	def prep (self):
 		Package.prep (self)
 		self.apply_extensions()
-		self.cd ('%{source_dir_name}.git')
+		self.cd ('%{source_dir_name}')
 		if Package.profile.name == 'darwin':
 			for p in range (2, len (self.sources)):
 				self.sh ('patch -p1 < "%{sources[' + str (p) + ']}"')
