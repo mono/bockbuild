13ce2058110f74b064bd41656bfc6dc7d2e6faf0
Martin Baulig <martin.baulig@xamarin.com>
2013-04-05 07:01:57 +0200

    Use git-readonly urls.

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -1,5 +1,5 @@
 Package ('gtk-sharp', '2-12-branch',
-	sources = [ 'git@github.com:mono/gtk-sharp.git' ],
+	sources = [ 'git://github.com/mono/gtk-sharp.git' ],
 	revision = '98e9439afa4b4da7cd2f3d1ab92b6415ab50b01d',
 	override_properties = { 'configure':
 		'./bootstrap-2.12 --prefix="%{prefix}"'
