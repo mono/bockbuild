5df0b646c58924c1c59665636c9b408596e85648
Duncan Mak <duncan@a-chinaman.com>
2013-04-07 20:47:59 -0400

    Fix sources

--- packages/gtk-sharp-2.12-release.py
+++ packages/gtk-sharp-2.12-release.py
@@ -1,8 +1,7 @@
 class GtkSharp212ReleasePackage (Package):
 	def __init__ (self):
-		Package.__init__ (self, 'gtk-sharp', '2.12.12')
+		Package.__init__ (self, 'gtk-sharp', '2.12.12', sources = ['http://files.xamarin.com/~alan/gtk-sharp-2.12.12.tar.gz'])
 		# self.configure = './bootstrap-2.12 --prefix="%{prefix}"'
-		self.sources = ['http://files.xamarin.com/~alan/gtk-sharp-2.12.12.tar.gz']
 		self.make = 'make CSC=gmcs'
 
 GtkSharp212ReleasePackage ()
