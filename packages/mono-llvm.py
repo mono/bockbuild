81489c115704c5df83ef7607121c8c20ab2ab2b0
Duncan Mak <duncan.mak@xamarin.com>
2013-04-08 17:04:35 -0400

    Set --build instead of --target.

--- packages/mono-llvm.py
+++ packages/mono-llvm.py
@@ -1,4 +1,4 @@
 GitHubTarballPackage ('mono', 'llvm', '3.0', '292aa8712c3120b03f9aa1d201b2e7949adf35c3',
-	configure = './configure --prefix="%{prefix}" --enable-optimized --enable-targets="x86 x86_64" --target=i386-apple-darwin10.8.0',
+	configure = './configure --prefix="%{prefix}" --enable-optimized --enable-targets="x86 x86_64" --build=i386-apple-darwin10.8.0',
 	override_properties = { 'make': 'make' }
 )
