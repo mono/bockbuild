da0316ba0f8a18cd50a2a70637242f15da175b2f
Duncan Mak <duncan@a-chinaman.com>
2013-04-07 22:22:26 -0400

    Set sources inside the __init__ constructor.

--- packages/nant.py
+++ packages/nant.py
@@ -1,8 +1,7 @@
 class NantPackage (Package):
 	def __init__(self):
-		Package.__init__(self, 'nant', '0.91')
-		self.sources = ['http://sourceforge.net/projects/nant/files/nant/0.91/nant-0.91-src.tar.gz']
-		
+		Package.__init__(self, 'nant', '0.91', sources = ['http://sourceforge.net/projects/nant/files/nant/0.91/nant-0.91-src.tar.gz'])
+
 	def build(self):
 		self.sh ('make install prefix="%{prefix}"')
 
