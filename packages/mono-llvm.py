b68ee952b5dc6f51d8faadc4010662116494e361
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-04-09 18:46:50 -0400

    [llvm] Parallel build seems to work now

--- packages/mono-llvm.py
+++ packages/mono-llvm.py
@@ -1,5 +1,4 @@
 GitHubPackage ('mono', 'llvm', '3.0',
 	revision = '292aa8712c3120b03f9aa1d201b2e7949adf35c3',
-	configure_flags  = ['--enable-optimized', '--enable-targets="x86 x86_64"', '--build=i386-apple-darwin10.8.0'],
-	override_properties = { 'make': 'make' }
+	configure_flags  = ['--enable-optimized', '--enable-targets="x86 x86_64"', '--build=i386-apple-darwin10.8.0']
 )
