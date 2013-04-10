2cc18973612cd34cc62b75a13a8b000710e86456
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-04-09 21:24:35 -0400

    Revert "[mono-addins] Use a commit that builds with current GTK#"
    This reverts commit fa252da773e9376f9c5134bdd8f7914c79ee8fd5.

--- packages/mono-addins.py
+++ packages/mono-addins.py
@@ -1,4 +1,5 @@
-GitHubPackage ('mono', 'mono-addins', '0.6.2',
-	'adcd75bb47ffc1665c8c410f44dad3511dec0da0',
-	configure = './autogen.sh --prefix="%{prefix}"'
-	)
+Package ('mono-addins', '0.6.2',
+	sources = [
+		'http://download.mono-project.com/sources/mono-addins/%{name}-%{version}.tar.bz2'
+	]
+)
