759ab537aa8fff1a116e6f8dd8ee82d6b845e188
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-04-23 16:42:22 -0400

    [gtksharp] Use GitHubPackage

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -1,5 +1,4 @@
-Package ('gtk-sharp', '2-12-branch',
-	sources = [ 'git://github.com/mono/gtk-sharp.git' ],
+GitHubPackage ('mono', 'gtk-sharp', '2-12-branch',
 	revision = '98e9439afa4b4da7cd2f3d1ab92b6415ab50b01d',
 	override_properties = { 'configure':
 		'./bootstrap-2.12 --prefix="%{prefix}"'
