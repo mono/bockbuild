0c7829310b071a52a1af4b6cd76413d27677b5b9
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-04-23 16:48:38 -0400

    [gtk+] Update

--- packages/gtk+.py
+++ packages/gtk+.py
@@ -1,6 +1,6 @@
 class GtkPackage (GnomeGitPackage):
 	def __init__ (self):
-		GnomeGitPackage.__init__ (self, 'gtk+', '2.24', 'd7e3f6f9f89f0acb7fd40179cc4246726deafef1',
+		GnomeGitPackage.__init__ (self, 'gtk+', '2.24', 'ba5ac33cdc18fe252de9baac6a2927f9ea77e6ab',
 			configure_flags = [
 				'--with-gdktarget=%{gdk_target}',
 #				'--disable-cups',
