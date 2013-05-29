22d5e1ae3716736d17fe4aee0bcb5195d0ef0d57
Rodrigo Kumpera <kumpera@gmail.com>
2013-05-29 16:16:15 -0400

    Bump libgdiplus to pick an OSX fix.

--- packages/libgdiplus.py
+++ packages/libgdiplus.py
@@ -1,1 +1,1 @@
-GitHubTarballPackage('mono', 'libgdiplus', '2.11', '3ddf7ff28e0876dc395368f11c0dd141199338f0', configure = './autogen.sh --prefix="%{prefix}"')
+GitHubTarballPackage('mono', 'libgdiplus', '2.11', 'b2fd6394f5604f2f6de66f07935b64ed575af48c', configure = './autogen.sh --prefix="%{prefix}"')
