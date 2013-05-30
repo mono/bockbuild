47da1593fd7fa49f5ed51d05187678bdaa53c06f
Rodrigo Kumpera <kumpera@gmail.com>
2013-05-30 12:47:13 -0400

    Properly configure fontconfig on the mac so we can find system fonts.

--- packages/fontconfig.py
+++ packages/fontconfig.py
@@ -1,6 +1,17 @@
-Package ('fontconfig', '2.10.0',
-	configure_flags = [ '--disable-docs' ],
-	sources = [
-		'http://www.fontconfig.org/release/%{name}-%{version}.tar.gz'
-	]
-)
+class FontConfigPackage (Package):
+	def __init__ (self):
+		Package.__init__ (self, 'fontconfig', '2.10.0',
+			configure_flags = [ '--disable-docs' ],
+			sources = [
+				'http://www.fontconfig.org/release/%{name}-%{version}.tar.gz'
+			])
+
+	def build (self):
+		if Package.profile.name == 'darwin':
+			self.configure_flags.extend ([
+				'--with-default-fonts=/System/Library/Fonts',
+				'--with-add-fonts=/Library/Fonts,/Network/Library/Fonts,/System/Library/Fonts,/usr/X11/lib/X11/fonts,/usr/X11R6/lib/X11/fonts,/usr/share/fonts'
+			])
+		Package.build (self)
+
+FontConfigPackage ()
