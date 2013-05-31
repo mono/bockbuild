d0446c40d9c7753afaf12edc3fd12bf19faf50d0
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-05-31 12:52:10 -0400

    [gtkimcocoa] Update for XS compat fix

--- packages/gtkimcocoa.py
+++ packages/gtkimcocoa.py
@@ -1,10 +1,10 @@
 Package ('gtkimcocoa', 'master',
-	sources = [ 'git://github.com/mhutch/gtkimcocoa.git' ],
+	sources = [ 'git://github.com/ashie/gtkimcocoa.git' ],
 	override_properties = {
 		'configure':
 			'./autogen.sh && ./configure --prefix=%{prefix}',
 		'makeinstall':
 			 'make install && gtk-query-immodules-2.0 > %{prefix}/etc/gtk-2.0/gtk.immodules',
 	},
-	revision = '663108828f8d1199a190b696815be80ff3e4b2ae',
+	revision = 'b5c4275187ac03c6a47a4fbcfd384069d0c37fad',
 )
