d8d7a433447185d4b790b31b34d2ba2550652b2f
Zoltan Varga <vargaz@gmail.com>
2013-06-27 13:22:45 +0200

    Bump llvm to f88c8bf5b31996435ed04b530cb4c7f02b244372.

--- packages/mono-llvm.py
+++ packages/mono-llvm.py
@@ -1,4 +1,4 @@
 GitHubPackage ('mono', 'llvm', '3.0',
-	revision = '292aa8712c3120b03f9aa1d201b2e7949adf35c3',
+	revision = 'f88c8bf5b31996435ed04b530cb4c7f02b244372',
 	configure_flags  = ['--enable-optimized', '--enable-targets="x86 x86_64"', '--build=i386-apple-darwin10.8.0']
 )
