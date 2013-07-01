675c927580dfb0d2c2847955181f2df879e0a23a
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-07-01 12:13:20 -0400

    Revert "[libpng] Bump to 1.6.2"
    This reverts commit de348f30d4fcd691aa5e8df23dc5543e11157bcb.

--- packages/libpng.py
+++ packages/libpng.py
@@ -1,7 +1,7 @@
 class LibPngPackage (Package):
 	def __init__ (self):
-		Package.__init__(self, 'libpng', '1.6.2',
-			sources = ['http://downloads.sourceforge.net/sourceforge/libpng/libpng-1.6.2.tar.xz'],
-			configure_flags = ['--enable-shared'])
+		Package.__init__(self, 'libpng', '1.4.12',
+										 sources = ['http://downloads.sourceforge.net/sourceforge/libpng/libpng-1.4.12.tar.xz'],
+										 configure_flags = ['--enable-shared'])
 
 LibPngPackage()
