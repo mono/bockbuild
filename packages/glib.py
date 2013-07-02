5d64c4300809c6c80f8f478cb0490c1c5369aac2
Martin Baulig <martin.baulig@xamarin.com>
2013-07-02 19:35:16 +0200

    Disable compile warnings to make this build with Xcode5.

--- packages/glib.py
+++ packages/glib.py
@@ -39,7 +39,7 @@ class GlibPackage (GnomeXzPackage):
 
 		self.sh (
 			# 'autoconf',
-			'%{configure}',
+			'%{configure} --disable-compile-warnings',
 			'ed - config.h < %{sources[1]}',
 			# work around https://bugzilla.gnome.org/show_bug.cgi?id=700350
 			'touch docs/reference/*/Makefile.in',
