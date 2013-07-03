55f2e7cbcb1c61f1e2dccc82db0d455104a376d7
Duncan Mak <duncanmak@gmail.com>
2013-07-03 10:41:23 -0400

    Remove the generation of the environment-generating script
    Will add it back in later.

--- profiles/mono-mac-xamarin/mono-mac-xamarin.py
+++ profiles/mono-mac-xamarin/mono-mac-xamarin.py
@@ -246,27 +246,3 @@ class MonoReleaseProfile(DarwinProfile, MonoReleasePackages):
 
 MonoReleaseProfile().build()
 
-profname = "mono-mac-release-env"
-dir = os.path.realpath(os.path.dirname(sys.argv[0]))
-envscript = '''#!/bin/sh
-PROFNAME="%s"
-INSTALLDIR=%s/build-root/_install
-export DYLD_FALLBACK_LIBRARY_PATH="$INSTALLDIR/lib:/lib:/usr/lib:$DYLD_FALLBACK_LIBRARY_PATH"
-export C_INCLUDE_PATH="$INSTALLDIR/include:$C_INCLUDE_PATH"
-export ACLOCAL_PATH="$INSTALLDIR/share/aclocal:$ACLOCAL_PATH"
-export ACLOCAL_FLAGS="-I $INSTALLDIR/share/aclocal $ACLOCAL_FLAGS"
-export PKG_CONFIG_PATH="$INSTALLDIR/lib/pkgconfig:$INSTALLDIR/lib64/pkgconfig:$INSTALLDIR/share/pkgconfig:$PKG_CONFIG_PATH"
-export CONFIG_SITE="$INSTALLDIR/$PROFNAME-config.site"
-export MONO_GAC_PREFIX="$INSTALLDIR:MONO_GAC_PREFIX"
-export MONO_ADDINS_REGISTRY="$INSTALLDIR/addinreg"
-export PATH="$INSTALLDIR/bin:$PATH"
-export MONO_INSTALL_PREFIX="$INSTALLDIR"
-
-#mkdir -p "$INSTALLDIR"
-#echo "test \"\$prefix\" = NONE && prefix=\"$INSTALLDIR\"" > $CONFIG_SITE
-
-PS1="[$PROFNAME] \w @ "
-''' % (profname, dir)
-
-with open(os.path.join(dir, profname), 'w') as f:
-    f.write(envscript)
