3cca074c46437d2e040d8c539d1d77f55cf4e3fc
Zoltan Varga <vargaz@gmail.com>
2013-09-19 22:35:08 +0200

    Bump LLVM to 8bdedb58a7a8d801c5b229b67f3de48e421928a1.

--- packages/mono-llvm.py
+++ packages/mono-llvm.py
@@ -1,7 +1,7 @@
 class MonoLlvmPackage (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'llvm', '3.0',
-			revision = '2c9642d011a72d2f264235b3bf7d31539b0aec04',
+			revision = '8bdedb58a7a8d801c5b229b67f3de48e421928a1',
 			configure_flags  = ['--enable-optimized', '--enable-targets="x86 x86_64"' ]
 		)
 
