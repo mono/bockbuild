14d429b7fc2b8beea92fdf5826848ad3588c7206
Martin Baulig <martin.baulig@xamarin.com>
2013-10-16 11:18:32 +0200

    Update PCL Reference Assemblies.
    These are now built and packaged by the 'mono-pcl' wrench lane,
    which also uploads them to storage.
    
    We're using the profiles that we got from Microsoft, but with
    SupportedFrameworks/ edited to add Xamarin.Android and Xamarin.iOS
    where appropriate.

--- packages/pcl-reference-assemblies.py
+++ packages/pcl-reference-assemblies.py
@@ -6,9 +6,9 @@ import shutil
 class PCLReferenceAssembliesPackage(Package):
     def __init__(self):
         Package.__init__(self,
-                         name='PortableReferenceAssemblies',
-                         version='2013-09-10',
-                         sources=['http://storage.bos.xamarin.com/bot-provisioning/mono-pcl-profiles.gm-2013-09-17.tar.gz'])
+                         name='mono-pcl-profiles',
+                         version='2013-10-15',
+                         sources=['http://storage.bos.xamarin.com/mono-pcl/e3/e3cf5cbc5b0607e29e0d68c67d36c8c9de91e0a1/mono-pcl-profiles.tar.gz'],
         self.source_dir_name = "mono-pcl-profiles"
 
     def prep(self):
