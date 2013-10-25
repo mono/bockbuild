b8f03556991cabab858bb31e5c8cb2f043ad14ce
Martin Baulig <martin.baulig@xamarin.com>
2013-10-25 20:02:44 +0200

    Use a versioned filename for the PCL profiles.

--- packages/pcl-reference-assemblies.py
+++ packages/pcl-reference-assemblies.py
@@ -6,9 +6,9 @@ import shutil
 class PCLReferenceAssembliesPackage(Package):
     def __init__(self):
         Package.__init__(self,
-                         name='mono-pcl-profiles',
-                         version='2013-10-23',
-                         sources=['http://storage.bos.xamarin.com/mono-pcl/58/5825e0404974d87799504a0df75ea4dca91f9bfe/mono-pcl-profiles.tar.gz'])
+                         name='mono-pcl-profiles-2013-10-25',
+                         version='2013-10-25',
+                         sources=['http://storage.bos.xamarin.com/bot-provisioning/mono-pcl-profiles-2013-10-25.tar.gz'])
         self.source_dir_name = "mono-pcl-profiles"
 
     def prep(self):
