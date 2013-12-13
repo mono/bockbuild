38df9cca7308e0e379a20cb68e047d6ab22fa7bb
Rodrigo Kumpera <kumpera@gmail.com>
2013-12-13 16:25:31 -0500

    Bump llvm to e656caccc7dfb5c51c208906f0e176f0973f030f.

--- packages/mono-llvm.py
+++ packages/mono-llvm.py
@@ -1,7 +1,7 @@
 class MonoLlvmPackage (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'llvm', '3.0',
-			revision = '8bdedb58a7a8d801c5b229b67f3de48e421928a1',
+			revision = 'e656caccc7dfb5c51c208906f0e176f0973f030f',
 			configure_flags  = ['--enable-optimized', '--enable-targets="x86 x86_64"' ]
 		)
 
