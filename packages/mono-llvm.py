c78f63ce4f25df2845b7b8625f31784efd2db357
Zoltan Varga <vargaz@gmail.com>
2014-03-31 16:34:37 -0400

    Pass -stlib=libc++ to LLVM configure.

--- packages/mono-llvm.py
+++ packages/mono-llvm.py
@@ -4,8 +4,11 @@ class MonoLlvmPackage (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'llvm', '3.0',
 			revision = '714a9d742af9ce911a3a46aa6e55041099a04be1',
-			configure_flags  = ['--enable-optimized', '--enable-assertions=no', '--enable-targets="x86 x86_64"' ]
+			configure_flags  = ['--enable-optimized', '--enable-assertions=no', '--enable-targets="x86 x86_64"']
 		)
+
+		if Package.profile.name == 'darwin':
+				self.configure_flags.extend (['CXXFLAGS=-stdlib=libc++']
 
 		if Package.profile.name == 'darwin' and not Package.profile.m64:
 				self.configure_flags.extend ([
