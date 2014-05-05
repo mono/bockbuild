82393028d65da87f6db15c896c6fd4312d909000
Alexis Christoforides <alexis@thenull.net>
2014-05-05 17:11:25 -0400

    Enable fat binaries on libpng package

--- packages/libpng.py
+++ packages/libpng.py
@@ -4,4 +4,17 @@ class LibPngPackage (Package):
 										 sources = ['http://downloads.sourceforge.net/sourceforge/libpng/libpng-1.4.12.tar.xz'],
 										 configure_flags = ['--enable-shared'])
 
+		#This package would like to be built with fat binaries
+		if Package.profile.m64 == True:
+			self.fat_build = True
+
+	def arch_build (self, arch):
+		if arch == 'darwin-fat': #multi-arch  build pass
+			self.local_ld_flags = ['-arch i386' , '-arch x86_64']
+			self.local_gcc_flags = ['-arch i386' , '-arch x86_64', '-Os']
+			self.local_configure_flags = ['--disable-dependency-tracking']
+
+		Package.arch_build (self, arch)
+
+
 LibPngPackage()
