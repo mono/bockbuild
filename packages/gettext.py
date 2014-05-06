c51067ee4182fb8f84c3cbeb8f7b017e28c51dfe
Alexis Christoforides <alexis@thenull.net>
2014-05-06 15:05:20 -0400

    Enabled fat binaries in cairo, get text packages, declared pkg-config as build dependency Cairo now builds fat binaries.

--- packages/gettext.py
+++ packages/gettext.py
@@ -20,6 +20,18 @@ class GettextPackage (GnuPackage):
 				'patches/gettext-no-samples.patch',
 			])
 
+		#This package would like to be built with fat binaries
+		if Package.profile.m64 == True:
+			self.fat_build = True
+
+	def arch_build (self, arch):
+		if arch == 'darwin-fat': #multi-arch  build pass
+			self.local_ld_flags = ['-arch i386' , '-arch x86_64']
+			self.local_gcc_flags = ['-arch i386' , '-arch x86_64']
+			self.local_configure_flags = ['--disable-dependency-tracking']
+
+		Package.arch_build (self, arch)
+
 	def prep (self):
 		Package.prep (self)
 		if Package.profile.name == 'darwin':
--- packages/pkg-config.py
+++ packages/pkg-config.py
@@ -1,1 +1,1 @@
-FreeDesktopPackage ('%{name}', 'pkg-config', '0.27', configure_flags = [ "--with-internal-glib" ])
+FreeDesktopPackage ('%{name}', 'pkg-config', '0.27', configure_flags = [ "--with-internal-glib" ], override_properties = { 'build_dependency' : True })
