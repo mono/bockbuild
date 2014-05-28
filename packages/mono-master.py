7c932a75189df7bf0183bfca4ff6263687e5fbf9
Alexis Christoforides <alexis@thenull.net>
2014-05-28 17:16:39 -0400

    Duplicate '--prefix' removed. Reinstate build parameters now that we've found what's wrong with the build.

--- packages/mono-master.py
+++ packages/mono-master.py
@@ -8,7 +8,6 @@ class MonoMasterPackage(Package):
 			revision = os.getenv('MONO_BUILD_REVISION'),
 			configure_flags = [
 				'--enable-nls=no',
-				'--prefix=' + Package.profile.prefix,
 				'--with-ikvm=yes',
 				'--with-moonlight=no'
 			]
@@ -38,15 +37,11 @@ class MonoMasterPackage(Package):
 				self.sh ('patch -p1 < "%{sources[' + str (p) + ']}"')
 
 	def arch_build (self, arch):	
-		if arch == 'darwin-64': #64-bit  build pass
-			#self.local_gcc_flags.extend (['-m64'])
-			#self.local_configure_flags = ['--build=x86_64-apple-darwin11.2.0']
-			#testing: the above flags should be benign but something is causing a faulty runtime to be built
-			self.local_gcc_flags = []
-			self.local_configure_flags = []
-			self.local.ld_flags = []
+		if arch == 'darwin-64': #64-bit build pass
+			self.local_gcc_flags.extend (['-m64'])
+			self.local_configure_flags = ['--build=x86_64-apple-darwin11.2.0']
 		
-		if arch == 'darwin-32':
+		if arch == 'darwin-32': #32-bit build pass
 			self.local_gcc_flags.extend (['-m32'])
 			self.local_configure_flags = ['--build=i386-apple-darwin11.2.0']
 
