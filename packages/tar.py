75095f30d1dbb7c35ccbfca1273efcf83f5d3152
Zoltan Varga <vargaz@gmail.com>
2014-06-08 20:48:22 +0200

    Build tar with --enable-nls=no, without this, it seems to crash when invoking gzip if LC_CTYPE/LANG are not set.

--- packages/tar.py
+++ packages/tar.py
@@ -1,1 +1,9 @@
-GnuPackage ('tar', '1.26', override_properties = { 'build_dependency' : True })
+class TarPackage (GnuPackage):
+	def __init__ (self):
+		GnuPackage.__init__ (self, 'tar', '1.26',
+			configure_flags = [
+				'--enable-nls=no'
+			]
+		)
+
+TarPackage ()
