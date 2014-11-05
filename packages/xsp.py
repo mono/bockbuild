f4838150750e502ac779a26d73e256b1541b3479
Zoltan Varga <vargaz@gmail.com>
2014-11-05 16:18:16 -0500

    Update xsp to 97f369b4512d54f9b8456171df4e93f990bf1f63 to work with mono master.

--- packages/xsp.py
+++ packages/xsp.py
@@ -1,1 +1,1 @@
-GitHubTarballPackage('mono', 'xsp', '3.0.11', '4587438369691b9b3e8415e1f113aa98b57d1fde', configure = './autogen.sh --prefix="%{prefix}"')  
+GitHubTarballPackage('mono', 'xsp', '3.0.11', '97f369b4512d54f9b8456171df4e93f990bf1f63', configure = './autogen.sh --prefix="%{prefix}"')
