0f6dcf8d5e989562a92bbc055837224fecad7f79
Zoltan Varga <vargaz@gmail.com>
2014-11-05 17:35:31 -0500

    Update xsp to 18686dcb247fb53611e5073efb630c9469d70d0e.

--- packages/xsp.py
+++ packages/xsp.py
@@ -1,1 +1,1 @@
-GitHubTarballPackage('mono', 'xsp', '3.0.11', '97f369b4512d54f9b8456171df4e93f990bf1f63', configure = './autogen.sh --prefix="%{prefix}"')
+GitHubTarballPackage('mono', 'xsp', '3.0.11', '18686dcb247fb53611e5073efb630c9469d70d0e', configure = './autogen.sh --prefix="%{prefix}"')
