c2947a7b660daf86d787e32dfb622924bb5bf4ea
Marek Safar <marek.safar@gmail.com>
2014-12-09 16:44:48 +0100

    Bump xsp hash

--- packages/xsp.py
+++ packages/xsp.py
@@ -1,1 +1,1 @@
-GitHubTarballPackage('mono', 'xsp', '3.0.11', 'bff467a4d7c426fe5b0dcb69af0c60696d7c4c57', configure = './autogen.sh --prefix="%{prefix}"')
+GitHubTarballPackage('mono', 'xsp', '3.0.11', 'e272a2c006211b6b03be2ef5bbb9e3f8fefd0768', configure = './autogen.sh --prefix="%{prefix}"')
