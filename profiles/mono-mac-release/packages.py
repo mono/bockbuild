599eaf68c5e4de505f572de18e685de00197f924
Zoltan Varga <vargaz@gmail.com>
2014-12-09 12:52:47 -0500

    Compile gtk# using mcs for mono master.

--- profiles/mono-mac-release/packages.py
+++ profiles/mono-mac-release/packages.py
@@ -67,7 +67,7 @@ class MonoReleasePackages:
 				'mono-master.py',
 				'libgdiplus.py',
 				'xsp.py',
-				'gtk-sharp-2.12-release.py',
+				'gtk-sharp-2.12-release-net4.5.py',
 				'boo.py',
 				# 'nant.py',
 				'ironlangs.py',
