42462135cec040d17f8ce4488c1ee6bb3b59f406
Alexis Christoforides <alexis@thenull.net>
2015-05-08 16:59:22 -0400

    Bump mono-basic to @mono/mono-basic/b8011b2f274606323da0927214ed98336465f467

--- packages/mono-basic.py
+++ packages/mono-basic.py
@@ -1,4 +1,4 @@
-GitHubTarballPackage ('mono', 'mono-basic', '3.0', '0d0440feccf648759f7316f93ad09b1e992ea13a',
+GitHubTarballPackage ('mono', 'mono-basic', '4.0.1', 'b8011b2f274606323da0927214ed98336465f467',
 	configure = './configure --prefix="%{prefix}"',
 	override_properties = { 'make': 'make' }
 )
