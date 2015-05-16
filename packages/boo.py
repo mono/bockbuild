3b7695958d38378dbcaa289ef18280f282dbb884
Zoltan Varga <vargaz@gmail.com>
2015-05-16 12:41:07 +0200

    Revert "Update boo source url to abf.io since codehaus is going away."
    This reverts commit 2491fbe5ce211523bf54fba1e1067062c1852e04.
    
    Revert this, the new url is not good.

--- packages/boo.py
+++ packages/boo.py
@@ -2,7 +2,7 @@ import os
 
 class BooPackage (Package):
 	def __init__(self):
-		Package.__init__ (self, 'boo', '0.9.4.9', sources = ['https://abf.io/current/boo/archive/boo-0.9.4.9-2.tar.gz'])
+		Package.__init__ (self, 'boo', '0.9.4.9', sources = ['http://dist.codehaus.org/boo/distributions/boo-0.9.4.9.tar.gz'])
 
 	def install(self):
 		# Unfortunately boo's build scripts don't seem to do a very good job 
