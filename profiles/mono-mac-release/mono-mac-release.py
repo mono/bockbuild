331cbbb685cea07ec63780a8f1f2efde4246dc93
Alexis Christoforides <alexis@thenull.net>
2015-05-21 15:36:44 -0400

    Allow profile class in mono-mac-release.py to be subclassed.

--- profiles/mono-mac-release/mono-mac-release.py
+++ profiles/mono-mac-release/mono-mac-release.py
@@ -295,29 +295,33 @@ class MonoReleaseProfile(DarwinProfile, MonoReleasePackages):
             x = x + 1
         print x
 
-MonoReleaseProfile().build()
+def main():
+    MonoReleaseProfile().build()
 
-profname = "mono-mac-release-env"
-dir = os.path.realpath(os.path.dirname(sys.argv[0]))
-envscript = '''#!/bin/sh
-PROFNAME="%s"
-INSTALLDIR=%s/build-root/_install
-export DYLD_FALLBACK_LIBRARY_PATH="$INSTALLDIR/lib:/lib:/usr/lib:$DYLD_FALLBACK_LIBRARY_PATH"
-export C_INCLUDE_PATH="$INSTALLDIR/include:$C_INCLUDE_PATH"
-export ACLOCAL_PATH="$INSTALLDIR/share/aclocal:$ACLOCAL_PATH"
-export ACLOCAL_FLAGS="-I $INSTALLDIR/share/aclocal $ACLOCAL_FLAGS"
-export PKG_CONFIG_PATH="$INSTALLDIR/lib/pkgconfig:$INSTALLDIR/lib64/pkgconfig:$INSTALLDIR/share/pkgconfig:$PKG_CONFIG_PATH"
-export CONFIG_SITE="$INSTALLDIR/$PROFNAME-config.site"
-export MONO_GAC_PREFIX="$INSTALLDIR:MONO_GAC_PREFIX"
-export MONO_ADDINS_REGISTRY="$INSTALLDIR/addinreg"
-export PATH="$INSTALLDIR/bin:$PATH"
-export MONO_INSTALL_PREFIX="$INSTALLDIR"
+    profname = "mono-mac-release-env"
+    dir = os.path.realpath(os.path.dirname(sys.argv[0]))
+    envscript = '''#!/bin/sh
+    PROFNAME="%s"
+    INSTALLDIR=%s/build-root/_install
+    export DYLD_FALLBACK_LIBRARY_PATH="$INSTALLDIR/lib:/lib:/usr/lib:$DYLD_FALLBACK_LIBRARY_PATH"
+    export C_INCLUDE_PATH="$INSTALLDIR/include:$C_INCLUDE_PATH"
+    export ACLOCAL_PATH="$INSTALLDIR/share/aclocal:$ACLOCAL_PATH"
+    export ACLOCAL_FLAGS="-I $INSTALLDIR/share/aclocal $ACLOCAL_FLAGS"
+    export PKG_CONFIG_PATH="$INSTALLDIR/lib/pkgconfig:$INSTALLDIR/lib64/pkgconfig:$INSTALLDIR/share/pkgconfig:$PKG_CONFIG_PATH"
+    export CONFIG_SITE="$INSTALLDIR/$PROFNAME-config.site"
+    export MONO_GAC_PREFIX="$INSTALLDIR:MONO_GAC_PREFIX"
+    export MONO_ADDINS_REGISTRY="$INSTALLDIR/addinreg"
+    export PATH="$INSTALLDIR/bin:$PATH"
+    export MONO_INSTALL_PREFIX="$INSTALLDIR"
 
-#mkdir -p "$INSTALLDIR"
-#echo "test \"\$prefix\" = NONE && prefix=\"$INSTALLDIR\"" > $CONFIG_SITE
+    #mkdir -p "$INSTALLDIR"
+    #echo "test \"\$prefix\" = NONE && prefix=\"$INSTALLDIR\"" > $CONFIG_SITE
 
-PS1="[$PROFNAME] \w @ "
-''' % (profname, dir)
+    PS1="[$PROFNAME] \w @ "
+    ''' % (profname, dir)
 
-with open(os.path.join(dir, profname), 'w') as f:
-    f.write(envscript)
+    with open(os.path.join(dir, profname), 'w') as f:
+        f.write(envscript)
+
+if __name__ == "__main__":
+    main()
