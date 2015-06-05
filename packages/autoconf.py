6bc4af73795beff1037bcbfedbeecfc61e0eb7c7
Alexis Christoforides <alexis@thenull.net>
2015-06-04 22:59:44 -0400

    Re-bundle autotools/libtool in package. The MonoDevelop build relies on them.

--- packages/autoconf.py
+++ packages/autoconf.py
@@ -1,1 +1,17 @@
-GnuPackage ('autoconf', '2.69', override_properties = { 'build_dependency' : True })
+class Autoconf (GnuPackage):
+	def __init__ (self):
+		GnuPackage.__init__ (self, 'autoconf', '2.69', override_properties = { 'build_dependency' : True })
+
+	def build (self):
+		Package.build (self)
+
+	def install (self):
+		Package.install (self)
+
+		#second build, to be bundled with the package
+		self.package_prefix = self.profile.staged_prefix
+		Package.build (self)
+		Package.install (self)
+
+Autoconf()
+
