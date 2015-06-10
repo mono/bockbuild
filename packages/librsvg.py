e742d56b9c9982d505f15ce2b427988310b57884
Alexis Christoforides <alexis@thenull.net>
2015-06-10 10:59:06 -0400

    Fix librsvg staging. Some files were being staged in the wrong path.

--- packages/librsvg.py
+++ packages/librsvg.py
@@ -1,2 +1,12 @@
-GnomeXzPackage ('librsvg', version_major = '2.37', version_minor = '0',
+class LibrsvgPackage(GnomeXzPackage):
+	def __init__ (self):
+		GnomeXzPackage.__init__(self, 'librsvg', version_major = '2.37', version_minor = '0',
         configure_flags = [ '--disable-Bsymbolic', '--disable-introspection' ])
+
+	def install (self):
+		Package.install (self)
+		# scoop up some mislocated files 
+		run_shell('rsync -a --ignore-existing %s/%s/* %s' % (self.profile.stage_root, self.profile.staged_prefix, self.profile.staged_prefix), True)
+		run_shell('rm -rf %s/%s/*' % (self.profile.stage_root, self.profile.staged_prefix), True)
+
+LibrsvgPackage()
