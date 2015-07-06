e6d2c3e376b810fcd70fbafecbf41d43957c6b2e
Stephan Sundermann <stephansundermann@gmail.com>
2015-07-07 00:40:13 +0200

    [packages] Update mono to v4.0.0
    For profiles that still want to stick with
    mono 2.x, there's still a mono-2-10.py package
    file.

--- packages/mono.py
+++ packages/mono.py
@@ -1,31 +1,26 @@
 class MonoPackage (Package):
 	def __init__ (self):
-		Package.__init__ (self, 'mono', '2.10.9',
+		Package.__init__ (self, 'mono', '4.0.0',
 			sources = [
 				'http://download.mono-project.com/sources/%{name}/%{name}-%{version}.tar.bz2',
-				'patches/mono-gtk-sharp-profiler.patch',
-				'patches/xbuild-bug4365.patch'
 			],
 			configure_flags = [
 				'--with-jit=yes',
 				'--with-ikvm=no',
 				'--with-mcs-docs=no',
 				'--with-moonlight=no',
-				'--enable-quiet-build',
+				'--enable-nls=no',
+				'--enable-quiet-build'
 			]
 		)
 		if Package.profile.name == 'darwin' and not Package.profile.m64:
 			self.configure_flags.extend ([
 				# fix build on lion, it uses 64-bit host even with -m32
-				'--build=i386-apple-darwin11.2.0',
+				#'--build=i386-apple-darwin11.2.0',
 			])
 
 		# Mono (in libgc) likes to fail to build randomly
 		self.make = 'for i in 1 2 3 4 5 6 7 8 9 10; do make && break; done'
-
-	def prep (self):
-		Package.prep (self)
-		self.sh ('patch -p1 < "%{sources[2]}"')
 
 	def install (self):
 		Package.install (self)
