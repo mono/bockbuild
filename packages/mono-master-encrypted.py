c7ea8bdccc19855f9a50ffd3a6b2ba0a759cac2b
Alexis Christoforides <alexis@thenull.net>
2015-07-29 19:21:12 -0400

    Added error handling to mono-extensions checkout

--- packages/mono-master-encrypted.py
+++ packages/mono-master-encrypted.py
@@ -43,18 +43,25 @@ class MonoMasterEncryptedPackage(Package):
 
         self.configure = './autogen.sh --prefix="%{package_prefix}"'
 
-    def checkout_mono_extensions(self, build_root):
+    def checkout_mono_extensions(self):
         ext = self.sources[1]
-        dirname = os.path.join(build_root, "mono-extensions")
+        dirname = os.path.join(self.profile.build_root, "mono-extensions")
+
         if not os.path.exists(dirname):
             self.sh('%' + '{git} clone --local --shared "%s" "%s"' % (ext, dirname))
-        self.cd(dirname)
-        self.sh('%{git} clean -xfd')
-        self.sh('%{git} fetch --all --prune')
-        if "pr/" not in os.getenv('MONO_BRANCH'):
-            self.sh('%' + '{git} checkout origin/%s' % os.getenv('MONO_BRANCH'))
-        else:
-            self.sh('%{git} checkout origin/master')
+        try:
+            self.pushd(dirname)
+            self.sh('%{git} clean -xfd')
+            self.sh('%{git} fetch --all --prune')
+            if "pr/" not in os.getenv('MONO_BRANCH'):
+                self.sh('%' + '{git} checkout origin/%s' % os.getenv('MONO_BRANCH'))
+            else:
+                self.sh('%{git} checkout origin/master')
+        except Exception as e:
+            self.popd (failure = True)
+            self.rm_if_exists (dirname)
+        finally:
+            self.popd ()
 
     def prep(self):
         Package.prep(self)
@@ -65,7 +72,7 @@ class MonoMasterEncryptedPackage(Package):
             self.rm (dest_dir)
 
         self.sh("ln -s %s %s" % (source_dir, dest_dir))
-        self.checkout_mono_extensions(self.profile.build_root)
+        retry (self.checkout_mono_extensions)
 
         self.cd(source_dir)
 
