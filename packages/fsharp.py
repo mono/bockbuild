1947b05119157cea75000fbcafe7057b4952df6d
Alexis Christoforides <alexis@thenull.net>
2015-07-31 13:32:52 -0400

    Try F# 4.0.0.1 again without the relocation patch

--- packages/fsharp.py
+++ packages/fsharp.py
@@ -5,17 +5,8 @@ class FsharpPackage(GitHubTarballPackage):
 			'4.0.0.1',
 			'c3aaf7d5f1f8905bbb3522b327703a43a0842cdf',
 			configure = './configure --prefix="%{package_prefix}"')
-		self.sources.extend ([
-			'patches/fsharp-fix-net45-profile.patch'])
 
 		self.extra_stage_files = ['lib/mono/xbuild/Microsoft/VisualStudio/v/FSharp/Microsoft.FSharp.Targets']
-
-	def prep(self):
-		Package.prep (self)
-
-		for p in range (1, len (self.sources)):
-				self.sh ('patch -p1 < "%{local_sources[' + str (p) + ']}"')
-
 
 	def build(self):
 		self.sh ('autoreconf')
