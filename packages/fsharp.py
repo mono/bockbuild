049b483ce5b02342dc1622aca0f963f63382eb4e
Alexis Christoforides <alexis@thenull.net>
2015-10-02 16:38:11 -0400

    fsharp: Bump to 4.0.0.3 and add El Capitan launcher script patch.

--- packages/fsharp.py
+++ packages/fsharp.py
@@ -2,11 +2,18 @@ class FsharpPackage(GitHubTarballPackage):
 	def __init__(self):
 		GitHubTarballPackage.__init__(self,
 			'fsharp', 'fsharp',
-			'4.0.0.1',
-			'c3aaf7d5f1f8905bbb3522b327703a43a0842cdf',
+			'4.0.0.3',
+			'14db8f5c19814dad05f51cf08e7ca718404e0e0a',
 			configure = './configure --prefix="%{package_prefix}"')
 
 		self.extra_stage_files = ['lib/mono/xbuild/Microsoft/VisualStudio/v/FSharp/Microsoft.FSharp.Targets']
+		self.sources.extend (['patches/fsharp-el-capitan-fix.patch']) # https://github.com/fsharp/fsharp/pull/495
+
+	def prep(self):
+		Package.prep (self)
+
+		for p in range (1, len (self.sources)):
+				self.sh ('patch -p1 < "%{local_sources[' + str (p) + ']}"')
 
 	def build(self):
 		self.sh ('autoreconf')
