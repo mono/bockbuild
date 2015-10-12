b6e6e1ac955072c9237caa1c60415be874ccca7e
Cody Russell <cody@jhu.edu>
2015-10-12 14:03:39 -0500

    [tar] Disable gl_cv_func_getcwd_abort_bug in configure
    On El Capitan this configure test creates thousands of nested
    subdirectories named `confdir-14B---` which can't be easily
    deleted.

--- packages/tar.py
+++ packages/tar.py
@@ -2,7 +2,8 @@ class TarPackage (GnuPackage):
 	def __init__ (self):
 		GnuPackage.__init__ (self, 'tar', '1.26',
 			configure_flags = [
-				'--enable-nls=no'
+				'--enable-nls=no',
+				'gl_cv_func_getcwd_abort_bug=no'
 			],
 			override_properties = { 'build_dependency' : True }
 		)
