3f776f7322090eef94bdb8c1326250f1aa2166f4
Zoltan Varga <vargaz@gmail.com>
2015-10-20 16:11:18 -0400

    Pass --enable-pecrypt to mono's configure, mono master now requires it.

--- packages/mono_crypto.py
+++ packages/mono_crypto.py
@@ -5,6 +5,6 @@ class MonoMasterEncryptedPackage (MonoMasterPackage):
     def __init__(self):
         MonoMasterPackage.__init__ (self)
 
-        self.configure_flags.extend(['--enable-extension-module=crypto --enable-native-types'])
+        self.configure_flags.extend(['--enable-extension-module=crypto --enable-native-types --enable-pecrypt'])
 
 MonoMasterEncryptedPackage()
