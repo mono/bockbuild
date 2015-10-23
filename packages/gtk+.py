dc2b3da28604e53e05132dde4fc5a71286544379
Cody Russell <cody@jhu.edu>
2015-10-23 08:44:27 -0500

    [gtk] Set dnd source context to NULL on finalize.

--- packages/gtk+.py
+++ packages/gtk+.py
@@ -161,7 +161,9 @@ class GtkPackage (GnomeGitPackage):
 				'patches/gtk/gtknsview-fix-invalid-casts.patch',
 
 				# https://bugzilla.xamarin.com/show_bug.cgi?id=29001
-				'patches/gtk/quartz-call-undo-redo-on-cmdz.patch'
+				'patches/gtk/quartz-call-undo-redo-on-cmdz.patch',
+
+				'patches/gtk/dnd-unset-context-on-finalize.patch'
 			])
 
 	def prep (self):
