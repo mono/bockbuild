aeacf02024888430715346b5979b4c5f663a0db4
Alexis Christoforides <alexis@thenull.net>
2015-12-16 11:45:24 -0500

    Bump F# to 4.0.1.1

--- packages/fsharp.py
+++ packages/fsharp.py
@@ -2,8 +2,8 @@ class FsharpPackage(GitHubTarballPackage):
 	def __init__(self):
 		GitHubTarballPackage.__init__(self,
 			'fsharp', 'fsharp',
-			'4.0.0.3',
-			'14db8f5c19814dad05f51cf08e7ca718404e0e0a',
+			'4.0.1.1',
+			'849e3061fd7db397f07c7bd0c08e5df19f2b712a',
 			configure = './configure --prefix="%{package_prefix}"')
 
 		self.extra_stage_files = ['lib/mono/xbuild/Microsoft/VisualStudio/v/FSharp/Microsoft.FSharp.Targets']
