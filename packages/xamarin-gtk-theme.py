644c5da6e0c30c2f8181abc48beaa38564ea50c9
Cody Russell <cody@jhu.edu>
2016-01-14 09:04:42 -0600

    Bump xamarin-gtk-theme

--- packages/xamarin-gtk-theme.py
+++ packages/xamarin-gtk-theme.py
@@ -2,7 +2,7 @@ class XamarinGtkThemePackage (Package):
 	def __init__ (self):
 		Package.__init__ (self, 'xamarin-gtk-theme',
 			sources = [ 'git://github.com/mono/xamarin-gtk-theme.git' ],
-			revision = '76918bd9319ba5e132291b908335108c37e875d2')
+			revision = '7749193e84e68697e81dd069cb6be712ff3dbff1')
 
 	def build (self):
 		try:
