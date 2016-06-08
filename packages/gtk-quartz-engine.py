73320ce58cdd2a1b47dec17904a748fc163a3c36
Cody Russell <cody@jhu.edu>
2016-06-08 10:08:29 -0500

    Bump gtk-quartz-engine
    This fixes a minor vertical alignment issue in checkboxes.
    
    https://github.com/mono/gtk-quartz-engine/pull/2

--- packages/gtk-quartz-engine.py
+++ packages/gtk-quartz-engine.py
@@ -6,6 +6,6 @@ class GtkQuartzEnginePackage (Package):
 				'configure': './autogen.sh --prefix=%{package_prefix}',
 				'needs_lipo' : True
 			},
-			revision = '355d5f123281e88e2ec5bc2e5093cee8b13923fb')
+			revision = '9555a08f0c9c98d02153c9d77b54a2dd83ce5d6f')
 
 GtkQuartzEnginePackage ()
