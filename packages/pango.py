040be3fa36e6bcf8afce0fcd428b1d3a766566e2
Cody Russell <cody@jhu.edu>
2016-06-14 14:25:43 -0500

    Revert "Check version, fix crasher on Yosemite."
    This reverts commit ab17254fe1593b74d6c9a8fe4c227354202394d8.
    
    This builds for me locally, but for some reason it's failing to build
    on wrench so I'm reverting until I can figure out why.

--- packages/pango.py
+++ packages/pango.py
@@ -34,8 +34,7 @@ class PangoPackage (GnomeXzPackage):
 			# https://bugzilla.xamarin.com/show_bug.cgi?id=32938
 			'patches/pango-coretext-fix-yosemite-crasher.patch',
 
-			'patches/pango-system-font-single.patch',
-			'patches/pango-system-font-check-version.patch'
+			'patches/pango-system-font-single.patch'
 		])
 
 	def prep (self):
