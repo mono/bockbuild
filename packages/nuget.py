def506cd2942ebdd4c4b6b54a2bff2ded2a6f3f9
Alexander Köplinger <alex.koeplinger@outlook.com>
2016-07-08 01:44:17 +0200

    NuGet: use GitHubPackage instead of GitHubTarballPackage
    We need an actual git checkout for the Makefile to work as it relies on submodules.

--- packages/nuget.py
+++ packages/nuget.py
@@ -1,7 +1,7 @@
 
-class NuGetPackage(GitHubTarballPackage):
+class NuGetPackage(GitHubPackage):
 	def __init__(self):
-		GitHubTarballPackage.__init__(self,
+		GitHubPackage.__init__(self,
 			'mono', 'nuget',
 			'2.12.0',
 			'9e2d2c1cc09d2a40eeb72e8c5db789e3b9bf2586',
