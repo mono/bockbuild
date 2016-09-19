4a753c3f7f3e146cc6be481637dc23cbf360ddc1
Marius Ungureanu <teromario@yahoo.com>
2016-09-19 15:33:34 +0300

    Switch gtk# to github and use latest 2-12.

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -1,7 +1,13 @@
 class GtkSharp212ReleasePackage (Package):
 	def __init__ (self):
-		Package.__init__ (self, 'gtk-sharp', '2.12.21', sources = ['http://download.mono-project.com/sources/gtk-sharp212/gtk-sharp-2.12.21.tar.gz'])
-		# self.configure = './bootstrap-2.12 --prefix="%{prefix}"'
+		Package.__init__ (self, 'gtk-sharp',
+			sources = ['git://github.com/mono/gtk-sharp.git'],
+			git_branch = 'gtk-sharp-2-12-branch',
+			revision = '9d2fd738322f7937352503fd3dd1fb8f1e5f0ff5',
+			override_properties = {
+				'configure': './bootstrap-2.12 --prefix=%{package_prefix}',
+			}
+		)
 		self.make = 'make CSC=mcs'
 
 GtkSharp212ReleasePackage ()
