bd431c7c420845e94d22ccb94759e64c94c66278
Marius Ungureanu <teromario@yahoo.com>
2016-09-23 02:43:53 +0300

    Bump gtk#

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -3,7 +3,7 @@ class GtkSharp212ReleasePackage (Package):
 		Package.__init__ (self, 'gtk-sharp',
 			sources = ['git://github.com/mono/gtk-sharp.git'],
 			git_branch = 'gtk-sharp-2-12-branch',
-			revision = '9dfddcdc2ee7ff4aa062926b9950b9c91fe130dc',
+			revision = '72637de6f2e519592f946a5acdf29a334c7bff75',
 			override_properties = {
 				'configure': './bootstrap-2.12 --prefix=%{package_prefix}',
 			}
