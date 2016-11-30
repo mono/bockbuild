mono/bockbuild

commit f42561e9ff8e60483897bb54e2191344556de94e

Improvements on clearing workspace files on error

bockbuild/package.py
@@ -34,6 +34,7 @@ class Package:
         self.gcc_flags = list(Package.profile.gcc_flags)
         self.cpp_flags = list(Package.profile.gcc_flags)
         self.ld_flags = list(Package.profile.ld_flags)
+        self.aux_files = [] # delete workspace-related files that are residing outside the workspace dir
 
         self.local_cpp_flags = []
         self.local_gcc_flags = []
@@ -583,16 +584,19 @@ class Package:
         except (Exception, KeyboardInterrupt) as e:
             self.rm_if_exists(self.stage_root)
             if isinstance(e, CommandException):
-                if os.path.exists(self.workspace) and self.is_local:
+                if os.path.exists(self.workspace) and not self.is_local:
+                    for path in self.aux_files:
+                        self.rm_if_exists(path)
                     problem_dir = os.path.join(
                         self.profile.bockbuild.execution_root, os.path.basename(self.workspace) + '.problem')
+
+                    shutil.move(self.workspace, problem_dir)
 
                     # take this chance to clear out older .problems
                     for d in os.listdir(self.profile.bockbuild.execution_root):
                         if d.endswith('.problem'):
                             self.rm(os.path.join(self.profile.bockbuild.execution_root, d))
 
-                    shutil.move(self.workspace, problem_dir)
                     info('Build moved to ./%s\n' % os.path.basename(problem_dir))
                 info('Run "source ./%s" first to replicate bockbuild environment.' %
                     os.path.basename(self.profile.bockbuild.env_script))

bockbuild/darwinprofile.py
@@ -121,8 +121,11 @@ class DarwinProfile (UnixProfile):
         else:
             error('Unknown arch %s' % arch)
 
+        configure_cache =  '%s/%s-%s.cache' % (self.bockbuild.build_root, package.name, arch)
+        package.aux_files.append (configure_cache)
+
         package.local_configure_flags.extend(
-            ['--cache-file=%s/%s-%s.cache' % (self.bockbuild.build_root, package.name, arch)])
+            ['--cache-file=%s' % configure_cache])
 
         if package.name in self.debug_info:
             package.local_gcc_flags.extend(['-g'])

commit 1764ddd3c198a0cf7986339694648cac273e0e9e

Fix another bug introduced by f42561e9ff8e60483897bb54e2191344556de94e

bockbuild.py
@@ -193,6 +193,7 @@ class Bockbuild:
             if os.path.exists(package.log):
                 delete(package.log)
 
+            package.source_dir_name = expand_macros(package.source_dir_name, package)
             dest = os.path.join(self.build_root, package.source_dir_name)
             package.fetch(dest)
 

bockbuild/package.py
@@ -143,7 +143,6 @@ class Package:
         scratch = self.profile.bockbuild.scratch
         resources = self.profile.bockbuild.resources
         source_cache_dir = self.profile.bockbuild.source_cache
-        self.source_dir_name = expand_macros(self.source_dir_name, self)
         self.buildstring = []
         self.is_local = False
         scratch_workspace = os.path.join(scratch, '%s.workspace' % self.name)