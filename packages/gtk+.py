39fa9e127c5edd4984202b769b72de825cc582c3
Cody Russell <cody@jhu.edu>
2017-02-09 11:26:29 -0600

    Fix oops

--- packages/gtk+.py
+++ packages/gtk+.py
@@ -187,7 +187,7 @@ class GtkPackage (GnomeGitPackage):
 
 				# https://bugzilla.xamarin.com/show_bug.cgi?id=51382
 				# https://bugzilla.xamarin.com/show_bug.cgi?id=51375
-				'patches/gtk/recompute-viewport-allocation-for-overlay-scrollbars.patch'
+				'patches/gtk/recompute-viewport-allocation-for-overlay-scrollbars.patch',
 
 				'patches/gtk/a11y-fix-dialog-accessibility.patch',
 				'patches/gtk/a11y-emit-container-add-when-inserting-menu.patch'
