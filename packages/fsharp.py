a8caa88a92a8eefe04b2853e921cb198cc2f6b91
nosami <jasonimison@gmail.com>
2017-04-10 23:40:14 +0100

    Adds missing symlinks for F# and support for portable pdb

--- packages/fsharp.py
+++ packages/fsharp.py
@@ -2,13 +2,16 @@ class FsharpPackage(GitHubTarballPackage):
 	def __init__(self):
 		GitHubTarballPackage.__init__(self,
 			'fsharp', 'fsharp',
-			'4.1.5',
-			'24eb0bc4bfdde9b4c12b604757ea8db209bf2aa3',
+			'4.1.8',
+			'29607f57efe83edeecd35070929fbdcf001b4d37',
 			configure = './configure --prefix="%{package_prefix}"',
 			override_properties = { 'make': 'make' })
 
 		self.extra_stage_files = ['lib/mono/xbuild/Microsoft/VisualStudio/v/FSharp/Microsoft.FSharp.Targets']
-		self.sources.extend (['patches/fsharp-fix-mdb-support.patch', 'patches/fsharp-fix-compile-after-issue.patch'])
+		self.sources.extend ([
+			'patches/fsharp-fix-mdb-support.patch',
+			'patches/fsharp-fix-compile-after-issue.patch',
+			'patches/fsharp-Add-missing-symlinks.patch'])
 
 	def prep(self):
 		Package.prep (self)
