144398b7c0991515f3f84559c694d36dd692a816
Marius Ungureanu <teromario@yahoo.com>
2017-05-31 12:26:30 +0300

    2.12.44

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -4,7 +4,7 @@ class GtkSharp212ReleasePackage (Package):
         Package.__init__(self, 'gtk-sharp',
                          sources=['git://github.com/mono/gtk-sharp.git'],
                          git_branch='gtk-sharp-2-12-branch',
-                         revision='62ad9c055ac0c7dfe0008508e1d39cac50150b45',
+                         revision='348ee1f3b0e2d08f7ed65d6b7425c99719c4930c',
                          override_properties={
                              'configure': './bootstrap-2.12 --prefix=%{package_prefix}',
                          }
