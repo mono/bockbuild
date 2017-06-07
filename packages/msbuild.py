74ebd4f643c9f3dd191fbc1cfdf1c56695a927d2
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-06-07 02:15:30 +0200

    Use explicit revision for msbuild

--- packages/msbuild.py
+++ packages/msbuild.py
@@ -3,6 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.3',
+			revision='f01689e45d2eba45cbb44fba69ebd20cc6438789',
 			git_branch = 'd15.3')
 
 	def build (self):
