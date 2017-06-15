5c0b5ff25a5ac6dcb6cb65971e1975fd2ee61e5f
Ankit Jain <ankit.jain@xamarin.com>
2017-06-15 02:01:52 -0400

    [msbuild] Update commit hash

--- packages/msbuild.py
+++ packages/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.3',
-			revision='f01689e45d2eba45cbb44fba69ebd20cc6438789',
+			revision='d48c73d627c40df95bd4c9d792333b8f211f13b5',
 			git_branch = 'd15.3')
 
 	def build (self):
