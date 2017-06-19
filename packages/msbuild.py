bc3742719c39742e397a5c4546c6f627b31a5909
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-06-19 15:13:03 +0200

    [msbuild] Update commit hash

--- packages/msbuild.py
+++ packages/msbuild.py
@@ -3,7 +3,7 @@ import fileinput
 class MSBuild (GitHubPackage):
 	def __init__ (self):
 		GitHubPackage.__init__ (self, 'mono', 'msbuild', '15.3',
-			revision='d48c73d627c40df95bd4c9d792333b8f211f13b5',
+			revision='735b8acdb3161abdf7e2c606e028eedf938043c8',
 			git_branch = 'd15.3')
 
 	def build (self):
