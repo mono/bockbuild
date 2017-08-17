4bb78ee802532c2917f1e6b4898b7f7a3deb6c3f
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-08-17 18:45:10 +0200

    Fix missing comma in gtk+.py

--- packages/gtk+.py
+++ packages/gtk+.py
@@ -199,10 +199,10 @@ class GtkPackage (GnomeGitPackage):
                 'patches/gtk/0001-A11y-Make-GtkTable-emit-the-container-add-signal.patch',
 
                 'patches/gtk/gtk-add-new-osx-versions.patch',
-                'patches/gtk/gtk-new-screen-updates-api.patch'
+                'patches/gtk/gtk-new-screen-updates-api.patch',
 
-		# https://bugzilla.xamarin.com/show_bug.cgi?id=5162
-		'patches/gtk/get-ascii-capable-keyboard-input-source.patch'
+                # https://bugzilla.xamarin.com/show_bug.cgi?id=5162
+                'patches/gtk/get-ascii-capable-keyboard-input-source.patch'
             ])
 
     def prep(self):
