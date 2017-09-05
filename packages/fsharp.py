191414e186ab18bf51c442f3b50ac14d6020df1f
nosami <jasonimison@gmail.com>
2017-09-05 12:40:32 +0100

    Apply Don's patch to remove non-IL or abstract method with non-zero RVA warning

--- packages/fsharp.py
+++ packages/fsharp.py
@@ -2,13 +2,13 @@ class FsharpPackage(GitHubTarballPackage):
 	def __init__(self):
 		GitHubTarballPackage.__init__(self,
 			'fsharp', 'fsharp',
-			'4.1.8',
+			'4.1.9',
 			'991186f6c95b30a80f217b9319354b32c86212de',
 			configure = './configure --prefix="%{package_prefix}"',
 			override_properties = { 'make': 'make' })
 
 		self.extra_stage_files = ['lib/mono/xbuild/Microsoft/VisualStudio/v/FSharp/Microsoft.FSharp.Targets']
-		self.sources.extend (['patches/fsharp-enable-jit-tracking-for-portable-pdb.patch', 'patches/fsharp-fix-mdb-support.patch', 'patches/fsharp-Fix-mono-gac-location.patch', 'patches/fsharp-fix-xbuild-check.patch'])
+		self.sources.extend (['patches/fsharp-enable-jit-tracking-for-portable-pdb.patch', 'patches/fsharp-fix-mdb-support.patch', 'patches/fsharp-Fix-mono-gac-location.patch', 'patches/fsharp-fix-xbuild-check.patch', 'patches/fsharp-remove-non-il-warning.patch'])
 
 	def prep(self):
 		Package.prep (self)
