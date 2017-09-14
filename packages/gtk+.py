c7c78ed20738ad2a189446f361578c84b09eb19b
Cody Russell <cody@jhu.edu>
2017-09-14 13:04:08 -0500

    Set new_layout to NULL

--- packages/gtk+.py
+++ packages/gtk+.py
@@ -205,7 +205,8 @@ class GtkPackage (GnomeGitPackage):
                 'patches/gtk/get-ascii-capable-keyboard-input-source.patch',
 
                 # https://developercommunity.visualstudio.com/content/problem/104471/visual-studio-for-mac-720540-cannot-launch-exc-bre.html
-                'patches/gtk/japanese-keyboard-input-source.patch'
+                'patches/gtk/japanese-keyboard-input-source.patch',
+		'patches/gtk/set_new_layout_null.patch'
             ])
 
     def prep(self):
