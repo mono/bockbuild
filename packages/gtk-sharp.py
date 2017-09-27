6ec9cf42ef50286478b3927d045eb5a9af41d76f
Cody Russell <cody@jhu.edu>
2017-09-26 19:57:37 -0500

    Try an older gtk# commit

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -3,8 +3,8 @@ class GtkSharp212ReleasePackage (Package):
     def __init__(self):
         Package.__init__(self, 'gtk-sharp',
                          sources=['git://github.com/mono/gtk-sharp.git'],
-                         git_branch='revert-string-marshalling',
-                         revision='c58035785d72b9ba4dbe9fe746d9e654ee74f905',
+                         git_branch='gtk-sharp-2-12-branch',
+                         revision='c7399c3e306cdf6ba0b6553a694434feff1823d4',
                          override_properties={
                              'configure': './bootstrap-2.12 --prefix=%{package_prefix}',
                          }
