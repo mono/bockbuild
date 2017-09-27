669e001e7cd92b9499f9f3e2d9f5c30b11d89d84
Cody Russell <cody@jhu.edu>
2017-09-26 23:55:25 -0500

    Go back

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -4,7 +4,7 @@ class GtkSharp212ReleasePackage (Package):
         Package.__init__(self, 'gtk-sharp',
                          sources=['git://github.com/mono/gtk-sharp.git'],
                          git_branch='gtk-sharp-2-12-branch',
-                         revision='c7399c3e306cdf6ba0b6553a694434feff1823d4',
+                         revision='7a6b8e38884246a4e9a69cf4f5179351319263a9',
                          override_properties={
                              'configure': './bootstrap-2.12 --prefix=%{package_prefix}',
                          }
