c950505a7cfa5e0ba26432352e58cacc4d85ca4f
Cody Russell <cody@jhu.edu>
2017-10-25 11:25:09 -0500

    Upgrade Pango to 1.40.12 and temporarily disable patches.

--- packages/pango.py
+++ packages/pango.py
@@ -3,8 +3,8 @@ class PangoPackage (GnomeXzPackage):
     def __init__(self):
         GnomePackage.__init__(self,
                               'pango',
-                              version_major='1.35',
-                              version_minor='0',
+                              version_major='1.40',
+                              version_minor='12',
                               configure_flags=[
                                   '--without-x',
                                   '--enable-gtk-doc-html=no'
@@ -39,11 +39,11 @@ class PangoPackage (GnomeXzPackage):
             'patches/pango-system-font-check-version.patch'
         ])
 
-    def prep(self):
-        GnomePackage.prep(self)
-        if Package.profile.name == 'darwin':
-            for p in range(1, len(self.local_sources)):
-                self.sh('patch -p1 < "%{local_sources[' + str(p) + ']}"')
+#    def prep(self):
+#       GnomePackage.prep(self)
+#        if Package.profile.name == 'darwin':
+#            for p in range(1, len(self.local_sources)):
+#                self.sh('patch -p1 < "%{local_sources[' + str(p) + ']}"')
 
     def deploy(self):
         self.sh('pango-querymodules --update-cache')
