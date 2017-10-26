3c7c348d0eacbe54dcac42f0fd4d92661253ea59
Cody Russell <cody@jhu.edu>
2017-10-26 10:04:54 -0500

    glib patches

--- packages/glib.py
+++ packages/glib.py
@@ -31,14 +31,14 @@ class GlibPackage (GnomeXzPackage):
                 'patches/glib-recursive-poll.patch',
             ])
 
-    # def prep(self):
-    #     Package.prep(self)
-    #     if self.darwin:
-    #         for p in range(2, 8):
-    #             self.sh('patch -p0 < %{local_sources[' + str(p) + ']}')
-    #         for p in range(8, len(self.local_sources)):
-    #             self.sh(
-    #                 'patch --ignore-whitespace -p1 < %{local_sources[' + str(p) + ']}')
+    def prep(self):
+        Package.prep(self)
+        if self.darwin:
+            for p in range(2, 8):
+                self.sh('patch -p0 < %{local_sources[' + str(p) + ']}')
+            for p in range(8, len(self.local_sources)):
+                self.sh(
+                    'patch --ignore-whitespace -p1 < %{local_sources[' + str(p) + ']}')
 
     def arch_build(self, arch):
         Package.profile.arch_build(arch, self)
