a3bdd26b91093c83b0f6084f325fbd73d324f221
Alexander Köplinger <alex.koeplinger@outlook.com>
2018-02-08 15:20:13 +0100

    Fix indentation

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -4,7 +4,7 @@ class GtkSharp212ReleasePackage (Package):
         Package.__init__(self, 'gtk-sharp',
                          sources=['git://github.com/mono/gtk-sharp.git'],
                          git_branch='gtk-sharp-2-12-branch',
-                     revision='372b03b5cf68af8088e1fec00c201ea057578021',
+                         revision='372b03b5cf68af8088e1fec00c201ea057578021',
                          override_properties={
                              'configure': './bootstrap-2.12 --prefix=%{package_prefix}',
                          }
