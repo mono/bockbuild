aef41b197bb6074d258e7f745d4503a06e5bd292
Marius Ungureanu <marius.ungureanu@xamarin.com>
2019-10-22 13:53:50 +0300

    [2018-08] Bump gtk# for clipboard binding fix

--- packages/gtk-sharp.py
+++ packages/gtk-sharp.py
@@ -4,7 +4,7 @@ class GtkSharp212ReleasePackage (Package):
         Package.__init__(self, 'gtk-sharp',
                          sources=['git://github.com/mono/gtk-sharp.git'],
                          git_branch='gtk-sharp-2-12-branch',
-                         revision='372b03b5cf68af8088e1fec00c201ea057578021',
+                         revision='a663d4ee3e8d97376bc4e561d9ed3fd7f31a6ace',
                          override_properties={
                              'configure': './bootstrap-2.12 --prefix=%{package_prefix}',
                          }
